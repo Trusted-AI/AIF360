f5485c3061ecf6e70f7993236982c2945b24e20c
Moninder Singh <39064734+monindersingh@users.noreply.github.com>
2018-12-11 12:46:35 -0500

    Features to drop bug fix (#60)
    fix bug in handling of features_to_drop

--- aif360/datasets/standard_dataset.py
+++ aif360/datasets/standard_dataset.py
@@ -95,7 +95,8 @@ class StandardDataset(BinaryLabelDataset):
         if instance_weights_name:
             keep |= set([instance_weights_name])
         df = df[sorted(keep - set(features_to_drop), key=df.columns.get_loc)]
-
+        categorical_features = sorted(set(categorical_features) - set(features_to_drop), key=df.columns.get_loc)
+        
         # 4. Remove any rows that have missing data.
         dropped = df.dropna()
         count = df.shape[0] - dropped.shape[0]
