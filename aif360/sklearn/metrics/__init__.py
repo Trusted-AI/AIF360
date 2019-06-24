57565074b9c52a2a943f5a6535be5113d3ed2d67
Samuel Hoffman <hoffman.sc@gmail.com>
2019-06-24 19:51:44 -0400

    added __all__ to __init__s

--- aif360/sklearn/metrics/__init__.py
+++ aif360/sklearn/metrics/__init__.py
@@ -1,1 +1,18 @@
-from aif360.sklearn.metrics.metrics import *
+from aif360.sklearn.metrics.metrics import consistency_score
+from aif360.sklearn.metrics.metrics import specificity_score
+from aif360.sklearn.metrics.metrics import selection_rate
+from aif360.sklearn.metrics.metrics import disparate_impact_ratio
+from aif360.sklearn.metrics.metrics import statistical_parity_difference
+from aif360.sklearn.metrics.metrics import equal_opportunity_difference
+from aif360.sklearn.metrics.metrics import average_odds_difference
+from aif360.sklearn.metrics.metrics import average_odds_error
+from aif360.sklearn.metrics.metrics import generalized_entropy_error
+from aif360.sklearn.metrics.metrics import between_group_generalized_entropy_error
+
+__all__ = [
+    'consistency_score', 'specificity_score', 'selection_rate',
+    'disparate_impact_ratio', 'statistical_parity_difference',
+    'equal_opportunity_difference', 'average_odds_difference',
+    'average_odds_error', 'generalized_entropy_error',
+    'between_group_generalized_entropy_error'
+]
