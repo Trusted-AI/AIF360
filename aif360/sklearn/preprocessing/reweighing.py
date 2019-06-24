8e52268d69a0739a757f29a9a2975b65eae61e77
Samuel Hoffman <hoffman.sc@gmail.com>
2019-06-24 10:58:46 -0400

    tweaks to reweighing to conform with sklearn

--- aif360/sklearn/preprocessing/reweighing.py
+++ aif360/sklearn/preprocessing/reweighing.py
@@ -1,11 +1,23 @@
-from warnings import warn
-
 import numpy as np
-from sklearn.base import BaseEstimator, TransformerMixin, MetaEstimatorMixin
-from sklearn.base import clone
+from pandas.core.dtypes.common import is_list_like
+from sklearn.base import BaseEstimator, MetaEstimatorMixin, clone
+from sklearn.utils import check_consistent_length
 from sklearn.utils.metaestimators import if_delegate_has_method
+from sklearn.utils.validation import column_or_1d, has_fit_parameter
 
-class Reweighing(BaseEstimator, TransformerMixin):
+
+def check_inputs(X, y, sample_weight):
+    if not hasattr(X, 'index'):
+        raise TypeError("Expected `DataFrame`, got {} instead.".format(type(X)))
+    y = column_or_1d(y)
+    if sample_weight is not None:
+        sample_weight = column_or_1d(sample_weight)
+    else:
+        sample_weight = np.ones(X.shape[0])
+    check_consistent_length(X, y, sample_weight)
+    return X, y, sample_weight
+
+class Reweighing(BaseEstimator):
     """Reweighing is a preprocessing technique that weights the examples in each
     (group, label) combination differently to ensure fairness before
     classification [#kamiran12]_.
@@ -34,65 +46,90 @@ class Reweighing(BaseEstimator, TransformerMixin):
            Techniques for Classification without Discrimination," Knowledge and
            Information Systems, 2012.
     """
-    # TODO: binary option for groups/labels?
-    def __init__(self):
-        self.sample_weight_ = np.empty(0)  # dynamic object for use in Pipeline
 
-    def fit(self, X, y=None):
-        raise NotImplementedError("Only 'fit_transform' is allowed.")
+    def __init__(self, prot_attr=None):
+        """
+        Args:
+            prot_attr (single label or list-like, optional): Protected
+                attribute(s) to use as sensitive attribute(s) in the reweighing
+                process. If more than one attribute, all combinations of values
+                (intersections) are considered. Default is `None` meaning all
+                protected attributes from the dataset are used.
+        """
+        self.prot_attr = prot_attr
 
-    def transform(self, X):
-        raise NotImplementedError("Only 'fit_transform' is allowed.")
+    def fit(self, X, y, sample_weight=None):
+        self.fit_transform(X, y, sample_weight=sample_weight)
+        return self
 
-    def fit_transform(self, X, y, groups, sample_weight=None):
+    def fit_transform(self, X, y, sample_weight=None):
         """Compute the factors for reweighing the dataset and transform the
         sample weights.
 
         Args:
             X (array-like): Training samples.
             y (array-like): Training labels.
-            groups (array-like): Protected attributes corresponding to samples.
             sample_weight (array-like, optional): Sample weights.
 
         Returns:
             X: Unchanged samples. Only the sample weights are different after
             transformation (see the `sample_weight_` attribute).
         """
-        if sample_weight is None:
-            sample_weight = np.ones(y.shape)
-        # resize all references (might be part of a Pipeline)
-        self.sample_weight_.resize(sample_weight.shape, refcheck=False)
+        X, y, sample_weight = check_inputs(X, y, sample_weight)
+
+        all_prot_attrs = X.index.names[1:]
+        if self.prot_attr is None:
+            self.prot_attr_ = all_prot_attrs
+        elif not is_list_like(self.prot_attr):
+            self.prot_attr_ = [self.prot_attr]
+        else:
+            self.prot_attr_ = self.prot_attr
+
+        if any(p not in X.index.names for p in self.prot_attr_):
+            raise ValueError("Some of the attributes provided are not present "
+                             "in the dataset. Expected a subset of:\n{}\nGot:\n"
+                             "{}".format(all_prot_attrs, self.prot_attr_))
+
+        self.sample_weight_ = np.empty_like(sample_weight)
+        groups = X.index.droplevel(list(set(X.index.names)
+                                      - set(self.prot_attr_))).to_flat_index()
+        # TODO: maintain categorical ordering
         self.groups_ = np.unique(groups)
         self.classes_ = np.unique(y)
+        n_groups = len(self.groups_)
+        n_classes = len(self.classes_)
+        self.reweigh_factors_ = np.full((n_groups, n_classes), np.nan)
 
         def N_(i): return sample_weight[i].sum()
-
         N = sample_weight.sum()
-        for g in self.groups_:
-            for c in self.classes_:
+        for i, g in enumerate(self.groups_):
+            for j, c in enumerate(self.classes_):
                 g_and_c = (groups == g) & (y == c)
                 if np.any(g_and_c):
                     W_gc = N_(groups == g) * N_(y == c) / (N * N_(g_and_c))
                     self.sample_weight_[g_and_c] = W_gc * sample_weight[g_and_c]
+                    self.reweigh_factors_[i, j] = W_gc
         return X
 
 
 class ReweighingMeta(BaseEstimator, MetaEstimatorMixin):
-    def __init__(self, estimator):
-        self.reweigher = Reweighing()
+    def __init__(self, estimator, reweigher=Reweighing()):
+        if not has_fit_parameter(estimator, 'sample_weight'):
+            raise TypeError("`estimator` (type: {}) does not have fit parameter"
+                            " `sample_weight`.".format(type(estimator)))
+        self.reweigher = reweigher
         self.estimator = estimator
 
-    def fit(self, X, y, pa_groups, sample_weight=None):
+    @property
+    def _estimator_type(self):
+        return self.estimator._estimator_type
+
+    def fit(self, X, y, sample_weight=None):
         self.reweigher_ = clone(self.reweigher)
         self.estimator_ = clone(self.estimator)
 
-        self.reweigher_.fit_transform(X, y, pa_groups, sample_weight=sample_weight)
-        try:
-            self.estimator_.fit(X, y, sample_weight=self.reweigher_.sample_weight_)
-        except TypeError:
-            raise ValueError("'estimator' ({}) does not incorporate "
-                             "'sample_weight' in 'fit()''.".format(
-                                     type(self.estimator_)))
+        self.reweigher_.fit_transform(X, y, sample_weight=sample_weight)
+        self.estimator_.fit(X, y, sample_weight=self.reweigher_.sample_weight_)
         return self
 
     @if_delegate_has_method('estimator')
@@ -107,7 +144,6 @@ class ReweighingMeta(BaseEstimator, MetaEstimatorMixin):
     def predict_log_proba(self, X):
         return self.estimator_.predict_log_proba(X)
 
-    # TODO: sample_weight isn't passed by GridSearchCV.score()
     @if_delegate_has_method('estimator')
     def score(self, X, y, sample_weight=None):
         return self.estimator_.score(X, y, sample_weight=sample_weight)
--- aif360/sklearn/tests/test_reweighing.py
+++ aif360/sklearn/tests/test_reweighing.py
@@ -1,53 +1,54 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score, make_scorer
 
-from aif360.datasets import GermanDataset
-from aif360.sklearn.datasets import fetch_german
+from aif360.datasets import AdultDataset
+from aif360.sklearn.datasets import fetch_adult
 from aif360.algorithms.preprocessing import Reweighing as OrigReweighing
 from aif360.sklearn.preprocessing import Reweighing, ReweighingMeta
 
 
-X, y = fetch_german(numeric_only=True, binary_age=True, dropcols='duration')
-german = GermanDataset(categorical_features=[], features_to_keep=[
-        'credit_amount', 'investment_as_income_percentage', 'residence_since',
-        'age', 'number_of_credits', 'people_liable_for', 'sex'])
-
-def test_dataset_equality():
-    assert (german.features == X.values).all()
+# X, y = fetch_german(numeric_only=True, dropcols='duration')
+# X.age = (X.age >= 25).astype('int')
+# german = GermanDataset(categorical_features=[], features_to_keep=[
+#         'credit_amount', 'investment_as_income_percentage', 'residence_since',
+#         'age', 'number_of_credits', 'people_liable_for', 'sex'])
+X, y, sample_weight = fetch_adult(numeric_only=True)
+adult = AdultDataset(instance_weights_name='fnlwgt', categorical_features=[],
+        features_to_keep=['age', 'education-num', 'capital-gain', 'capital-loss',
+                          'hours-per-week'], features_to_drop=[])
 
 def test_reweighing_sex():
     orig_rew = OrigReweighing(unprivileged_groups=[{'sex': 0}],
                               privileged_groups=[{'sex': 1}])
-    german_fair = orig_rew.fit_transform(german)
-    rew = Reweighing()
-    rew.fit_transform(X, y, groups=X.index.get_level_values('sex'))
+    adult_fair = orig_rew.fit_transform(adult)
+    rew = Reweighing('sex')
+    rew.fit_transform(X, y, sample_weight=sample_weight)
 
-    assert np.allclose(german_fair.instance_weights, rew.sample_weight_)
+    # assert np.allclose([[orig_rew.w_up_unfav, orig_rew.w_up_fav],
+    #                     [orig_rew.w_p_unfav, orig_rew.w_p_fav]],
+    #                    rew.reweigh_factors_)
+    assert np.allclose(adult_fair.instance_weights, rew.sample_weight_)
 
 def test_reweighing_intersection():
     rew = Reweighing()
-    rew.fit_transform(X, y, groups=X.index.to_flat_index())
-    assert len(rew.groups_) == 4
-    assert len(rew.classes_) == 2
-
-def test_pipeline():
-    logreg = LogisticRegression(solver='liblinear')
-    pipe = make_pipeline(Reweighing(), logreg)
-    fit_params = {'logisticregression__sample_weight': pipe[0].sample_weight_,
-                  'reweighing__groups': X.index.get_level_values('sex')}
-    pipe.fit(X, y, **fit_params)
-    assert (logreg.fit(X, y, sample_weight=pipe[0].sample_weight_).coef_
-         == pipe[-1].coef_).all()
+    rew.fit_transform(X, y)
+    assert rew.reweigh_factors_.shape == (4, 2)
 
 def test_gridsearch():
-    rew = ReweighingMeta(LogisticRegression(solver='liblinear'))
-    params = {'estimator__C': [1, 10]}
-    clf = GridSearchCV(rew, params, cv=5)
-    # TODO: 'groups' name clashes with CV splitter
-    fit_params = {'pa_groups': X.index.get_level_values('sex'),
-                  'sample_weight': np.random.random(y.shape)}
-    clf.fit(X, y, **fit_params)
-    # print(clf.score(X, y))
-    assert len(clf.best_estimator_.reweigher_.groups_) == 2
+    # logreg = LogisticRegression(solver='lbfgs', max_iter=500)
+    # rew = ReweighingMeta(estimator=logreg, reweigher=Reweighing('sex'))
+    rew = ReweighingMeta(estimator=LogisticRegression(solver='liblinear'))
+
+    # UGLY workaround for sklearn issue: https://stackoverflow.com/a/49598597
+    def score_func(y_true, y_pred, sample_weight):
+        idx = y_true.index.to_flat_index()
+        return accuracy_score(y_true, y_pred, sample_weight=sample_weight[idx])
+    scoring = make_scorer(score_func, **{'sample_weight': sample_weight})
+
+    params = {'estimator__C': [1, 10], 'reweigher__prot_attr': ['sex']}
+
+    clf = GridSearchCV(rew, params, scoring=scoring, cv=5, iid=False)
+    clf.fit(X, y, **{'sample_weight': sample_weight})
+    # print(clf.best_score_)
