0ce0f6fca983ab5b26642674baf47de12bb04e3d
Seth Neel <sethneel@Seths-MacBook-Pro.local>
2019-10-18 14:42:24 -0400

    examples wip

--- examples/gerryfair-ex/different_classifiers.py
+++ examples/gerryfair-ex/different_classifiers.py
@@ -1,20 +1,14 @@
-
 from aif360.algorithms.inprocessing.gerryfair_classifier import Model
-from aif360.algorithms.inprocessing.gerryfair.clean import *
 from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import load_preproc_data_adult
 from sklearn import svm
 from sklearn import tree
 from sklearn.kernel_ridge import KernelRidge
 from sklearn import linear_model
-
 import pickle
 import matplotlib.pyplot as plt
 
-dataset_orig = load_preproc_data_adult()
-X, X_prime, y = extract_df_from_ds(dataset_orig)
 
-
-def single_trial(predictor='kernel', C=15, printflag=True, gamma=0.1, max_iter=10):
+def single_trial(dataset, predictor='kernel', C=15, printflag=True, gamma=0.1, max_iter=10):
     if predictor == 'kernel':
         model = KernelRidge(alpha=1.0, gamma=1.0, kernel='rbf')
     elif predictor == 'tree':
@@ -24,10 +18,10 @@ def single_trial(predictor='kernel', C=15, printflag=True, gamma=0.1, max_iter=1
     else:
         model = linear_model.LinearRegression()
     fair_clf = Model(C=C, printflag=printflag, gamma=gamma, predictor=model, max_iters=max_iter)
-    fair_clf.fit(dataset_orig)
+    fair_clf.fit(dataset)
 
 
-def multiple_comparision(gamma_list=tuple([0.01]), max_iter_list=tuple([5]), C=15):
+def multiple_comparision(dataset, gamma_list=tuple([0.01]), max_iter_list=tuple([5]), C=15):
     ln_predictor = linear_model.LinearRegression()
     svm_predictor = svm.LinearSVR()
     tree_predictor = tree.DecisionTreeRegressor()
@@ -46,19 +40,15 @@ def multiple_comparision(gamma_list=tuple([0.01]), max_iter_list=tuple([5]), C=1
             predictor = predictor_dict[curr_predictor]
             fair_clf = Model(C=C, printflag=printflag, gamma=1, predictor=predictor, max_iters=max_iter)
             print(fair_clf.predictor)
-            all_errors, all_fp = fair_clf.pareto(X, X_prime, y, gamma_list)
+            all_errors, all_fp = fair_clf.pareto(dataset, gamma_list)
             results_dict[curr_predictor] = {'Errors' : all_errors, 'FP_disp': all_fp}
+    # pickle.dump(results_dict, open('results_max' + str(max_iter_list) + '_gammas' + str(gamma_list) + '.pkl', 'wb'))
 
     print(results_dict)
-    #pickle.dump(results_dict, open('results_max' + str(max_iter_list) + '_gammas' + str(gamma_list) + '.pkl', 'wb'))
 
 
-def multiple_pareto(gamma_list=[0.002, 0.005, 0.01, 0.02, 0.05, 0.1], save_results=True):
+def multiple_pareto(dataset, gamma_list=[0.002, 0.005, 0.01, 0.02, 0.05, 0.1], save_results=True):
 
-    train_size = X.shape[0]
-    X_train = X.iloc[:train_size]
-    X_prime_train = X_prime.iloc[:train_size]
-    y_train = y.iloc[:train_size]
 
     ln_predictor = linear_model.LinearRegression()
     svm_predictor = svm.LinearSVR()
@@ -77,7 +67,7 @@ def multiple_pareto(gamma_list=[0.002, 0.005, 0.01, 0.02, 0.05, 0.1], save_resul
         max_iters = predictor_dict[pred]['iters']
         fair_clf = Model(C=100, printflag=True, gamma=1, predictor=predictor, max_iters=max_iters)
         fair_clf.set_options(max_iters=max_iters)
-        errors, fp_violations, fn_violations = fair_clf.pareto(X_train, X_prime_train, y_train, gamma_list)
+        errors, fp_violations, fn_violations = fair_clf.pareto(dataset, gamma_list)
         results_dict[pred] = {'errors': errors, 'fp_violations': fp_violations, 'fn_violations': fn_violations}
         plt.plot(errors, fp_violations, label=pred)
 
@@ -90,4 +80,7 @@ def multiple_pareto(gamma_list=[0.002, 0.005, 0.01, 0.02, 0.05, 0.1], save_resul
     plt.title('Error vs. Unfairness\n(Communities & Crime Dataset)')
     plt.show()
 
-multiple_pareto()
+
+if __name__ == '__main__':
+    dataset = load_preproc_data_adult()
+    multiple_pareto(dataset)
