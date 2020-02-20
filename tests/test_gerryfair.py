4fa837a5eda4123aedf2e318aeec3faf5902ad78
svnscholar <sethneel@wharton.upenn.edu>
2020-02-19 19:32:21 -0500

    testing via running short notebook

--- tests/test_gerryfair.py
+++ tests/test_gerryfair.py
@@ -1,31 +1,14 @@
-from aif360.algorithms.inprocessing.gerryfair_classifier import *
-from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import *
-import pytest
-from aif360.metrics.binary_label_dataset_metric import *
-
-@pytest.fixture
-def gerry_model():
-    max_iterations = 100
-    C = 100
-    print_flag = True
-    gamma = .005
-
-    gerry_model = GerryFairClassifier(C=C, printflag=print_flag, gamma=gamma, fairness_def='FP',
-                                     max_iters=max_iterations, heatmapflag=False)
-    return gerry_model
+from notebook_runner import notebook_run
+import os
 
 
-@pytest.fixture
-def adult_dataset():
-    adult_dataset = load_preproc_data_adult(sub_samp=1000, balance=True)
-    return adult_dataset
+def test_gerryfair_classifier():
+    nb, errors = notebook_run(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '..', 'examples', 'demo_short_gerryfair_test.ipynb'))
 
-# test train, predict, audit
-def test_auditor(adult_dataset, gerry_model):
-    gerry_model.fit(adult_dataset, early_termination=True)
-    dataset_predicted = gerry_model.predict(adult_dataset, threshold=False)
-    gerry_metric = BinaryLabelDatasetMetric(adult_dataset)
-    gamma_disparity = gerry_metric.rich_subgroup(dataset_predicted.labels, 'FP')
-    assert gamma_disparity >= 0
-    assert gamma_disparity <= 1
-
+    if len(errors) > 0:
+        for err in errors:
+            for tbi in err['traceback']:
+                print(tbi)
+        raise AssertionError("errors in notebook testcases")
