b136c174c3e1b63b8bdf571dd3ab4023d5266047
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2020-03-09 17:13:23 -0700

    Update setup.py
    Are removing bias our mitigating bias?

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-long_description = """The AI Fairness 360 toolkit is an open-source library to help detect and remove bias in machine
+long_description = """The AI Fairness 360 toolkit is an open-source library to help detect and mitigate bias in machine
 learning models. The AI Fairness 360 Python package includes a comprehensive set of metrics for datasets and models to
 test for biases, explanations for these metrics, and algorithms to mitigate bias in datasets and models.
 
