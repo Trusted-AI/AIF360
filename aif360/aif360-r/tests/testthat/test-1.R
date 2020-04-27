171541c8542b21cb59baad0c0a181c9879b67ca8
saishruthi <saishruthi.tn@ibm.com>
2020-04-27 08:27:51 -0700

    update travis

--- aif360/aif360-r/tests/testthat/test-1.R
+++ aif360/aif360-r/tests/testthat/test-1.R
@@ -1,3 +1,5 @@
 # load aif library
+library(reticulate)
 install_aif360(method='virtualenv', envname='check-pack', conda_python_version = '3.7')
+reticulate::use_virtualenv('check-pack')
 load_aif360_lib()
