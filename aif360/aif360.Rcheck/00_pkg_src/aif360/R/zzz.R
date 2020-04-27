b71bd25ce1d55c38f865b442256f2238378b53b4
saishruthi <saishruthi.tn@ibm.com>
2020-04-27 02:36:50 -0700

    update pandas

--- aif360/aif360.Rcheck/00_pkg_src/aif360/R/zzz.R
+++
@@ -1,4 +0,0 @@
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")
-    utils::globalVariables(c("datasets", "metrics", "tf", "pre_algo", "in_algo", "post_algo"))
-Globals <- list()
