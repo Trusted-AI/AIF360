7d050d32a050c355436693d1f9e828e02a66732b
saishruthi <saishruthi.tn@ibm.com>
2020-04-30 09:39:10 -0700

    merge

--- aif360/aif360-r/inst/examples/test.R
+++ aif360/aif360-r/inst/examples/test.R
@@ -1,4 +1,4 @@
-library(raif360)
+library(aif360)
 install_aif360()
 
 
@@ -6,13 +6,15 @@ load_aif360_lib()
 
 
 
-dd <- raif360::aif_dataset(data_path='.... path to the pre_processed.csv input file',
-                           favor_label=0,
-                           unfavor_label=1,
-                           unprivileged_protected_attribute=1,
-                           privileged_protected_attribute=0,
-                           target_column="Attrition",
-                           protected_attribute="OverTime")
+dd <- aif360::aif_dataset(
+  data_path = system.file("extdata", "data.csv", package="aif360"),
+  favor_label=0,
+  unfavor_label=1,
+  unprivileged_protected_attribute=0,
+  privileged_protected_attribute=1,
+  target_column="income",
+  protected_attribute="sex")
+
 dd$favorable_label
 dd$labels
 dd$unfavorable_label
