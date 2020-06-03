0c42a47a6dff415d68f7db3bc5e4d8dac0d8ede8
saishruthi <saishruthi.tn@ibm.com>
2020-06-03 13:58:58 -0700

    update check for cran

--- aif360/aif360-r/tests/testthat/test-1.R
+++ aif360/aif360-r/tests/testthat/test-1.R
@@ -1,2 +1,15 @@
-# load aif library
-load_aif360_lib()
+# helper function to skip tests if we don't have the 'foo' module
+skip_if_no_py_modules <- function() {
+  have_scipy <- reticulate::py_module_available("aif360")
+  if (!have_scipy)
+    skip("AIF360 not available for testing")
+}
+
+# then call this function from all of your tests
+test_that("Things work as expected", {
+  skip_if_no_py_modules()
+  # load aif library
+  load_aif360_lib()
+})
+
+
