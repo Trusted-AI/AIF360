c9ddc1db4a3bbffd762916ad63f4f28ba21c165b
Prasanna Sattigeri <psattig@Prasannas-MacBook-Pro.local>
2020-06-20 14:52:33 -0400

    lfr cleanup and update.

--- aif360/algorithms/preprocessing/lfr.py
+++ aif360/algorithms/preprocessing/lfr.py
@@ -29,7 +29,7 @@ class LFR(Transformer):
                  Ay=1.0,
                  Az=50.0,
                  print_interval=250,
-                 verbose=1,
+                 verbose=0,
                  seed=None):
         """
         Args:
@@ -67,7 +67,8 @@ class LFR(Transformer):
         self.print_interval = print_interval
         self.verbose = verbose
 
-        self.learned_model = None
+        self.w = None
+        self.prototypes = None
 
     def fit(self, dataset, maxiter=5000, maxfun=5000, **kwargs):
         """Compute the transformation parameters that leads to fair representations.
@@ -81,30 +82,35 @@ class LFR(Transformer):
 
         num_train_samples, self.features_dim = np.shape(dataset.features)
 
-        d = np.reshape(
+        protected_attributes = np.reshape(
             dataset.protected_attributes[:, dataset.protected_attribute_names.index(self.protected_attribute_name)],
             [-1, 1])
-        sensitive_idx = np.array(np.where(d == self.unprivileged_group_protected_attribute_value))[0].flatten()
-        nonsensitive_idx = np.array(np.where(d == self.privileged_group_protected_attribute_value))[0].flatten()
-        training_sensitive = dataset.features[sensitive_idx]
-        training_nonsensitive = dataset.features[nonsensitive_idx]
-        ytrain_sensitive = dataset.labels[sensitive_idx]
-        ytrain_nonsensitive = dataset.labels[nonsensitive_idx]
+        unprivileged_sample_ids = np.array(np.where(protected_attributes == self.unprivileged_group_protected_attribute_value))[0].flatten()
+        privileged_sample_ids = np.array(np.where(protected_attributes == self.privileged_group_protected_attribute_value))[0].flatten()
+        features_unpriveleged = dataset.features[unprivileged_sample_ids]
+        features_priveleged = dataset.features[privileged_sample_ids]
+        labels_unpriveleged = dataset.labels[unprivileged_sample_ids]
+        labels_priveleged = dataset.labels[privileged_sample_ids]
 
-        model_inits = np.random.uniform(size=self.features_dim * 2 + self.k + self.features_dim * self.k)
+        # Initialize the LFR optim objective parameters
+        parameters_initialization = np.random.uniform(size=self.k + self.features_dim * self.k)
         bnd = []
-        for i, _ in enumerate(model_inits):
+        for i, _ in enumerate(parameters_initialization):
             if i < self.features_dim * 2 or i >= self.features_dim * 2 + self.k:
                 bnd.append((None, None))
             else:
                 bnd.append((0, 1))
+        lfr_helpers.LFR_optim_objective.steps = 0
 
-        self.learned_model = optim.fmin_l_bfgs_b(lfr_helpers.LFR_optim_obj, x0=model_inits, epsilon=1e-5,
-                                  args=(training_sensitive, training_nonsensitive,
-                                        ytrain_sensitive[:, 0], ytrain_nonsensitive[:, 0], self.k, self.Ax,
-                                        self.Ay, self.Az, 0, self.print_interval),
-                                  bounds=bnd, approx_grad=True, maxfun=maxfun,
-                                  maxiter=maxiter, disp=self.verbose)[0]
+        learned_parameters = optim.fmin_l_bfgs_b(lfr_helpers.LFR_optim_objective, x0=parameters_initialization, epsilon=1e-5,
+                                                      args=(features_unpriveleged, features_priveleged,
+                                        labels_unpriveleged[:, 0], labels_priveleged[:, 0], self.k, self.Ax,
+                                        self.Ay, self.Az, self.print_interval),
+                                                      bounds=bnd, approx_grad=True, maxfun=maxfun,
+                                                      maxiter=maxiter, disp=self.verbose)[0]
+        self.w = learned_parameters[:self.k]
+        self.prototypes = learned_parameters[self.k:].reshape((self.k, self.features_dim))
+
         return self
 
     def transform(self, dataset, threshold=0.5, **kwargs):
@@ -118,54 +124,28 @@ class LFR(Transformer):
         if self.seed is not None:
             np.random.seed(self.seed)
 
-        num_test_samples, _ = np.shape(dataset.features)
-
-        d = np.reshape(
+        protected_attributes = np.reshape(
             dataset.protected_attributes[:, dataset.protected_attribute_names.index(self.protected_attribute_name)],
             [-1, 1])
-        sensitive_idx = np.array(np.where(d == self.unprivileged_group_protected_attribute_value))[0].flatten()
-        nonsensitive_idx = np.array(np.where(d == self.privileged_group_protected_attribute_value))[0].flatten()
-        testing_sensitive = dataset.features[sensitive_idx]
-        testing_nonsensitive = dataset.features[nonsensitive_idx]
-        ytest_sensitive = dataset.labels[sensitive_idx]
-        ytest_nonsensitive = dataset.labels[nonsensitive_idx]
+        unprivileged_sample_ids = \
+        np.array(np.where(protected_attributes == self.unprivileged_group_protected_attribute_value))[0].flatten()
+        privileged_sample_ids = \
+        np.array(np.where(protected_attributes == self.privileged_group_protected_attribute_value))[0].flatten()
+        features_unpriveleged = dataset.features[unprivileged_sample_ids]
+        features_priveleged = dataset.features[privileged_sample_ids]
+        labels_unpriveleged = dataset.labels[unprivileged_sample_ids]
+        labels_priveleged = dataset.labels[privileged_sample_ids]
 
-        # extract training model parameters
-        Ns, P = testing_sensitive.shape
-        N, _ = testing_nonsensitive.shape
-        alphaoptim0 = self.learned_model[:P]
-        alphaoptim1 = self.learned_model[P: 2 * P]
-        woptim = self.learned_model[2 * P: (2 * P) + self.k]
-        voptim = np.matrix(self.learned_model[(2 * P) + self.k:]).reshape((self.k, P))
+        _, features_hat_unpriveleged, labels_hat_unpriveleged = lfr_helpers.get_xhat_y_hat(self.prototypes, self.w, features_unpriveleged)
 
-        # compute distances on the test dataset using train model params
-        dist_sensitive = lfr_helpers.distances(testing_sensitive, voptim, alphaoptim1, Ns, P, self.k)
-        dist_nonsensitive = lfr_helpers.distances(testing_nonsensitive, voptim, alphaoptim0, N, P, self.k)
-
-        # compute cluster probabilities for test instances
-        M_nk_sensitive = lfr_helpers.M_nk(dist_sensitive, Ns, self.k)
-        M_nk_nonsensitive = lfr_helpers.M_nk(dist_nonsensitive, N, self.k)
-
-        # learned mappings for test instances
-        res_sensitive = lfr_helpers.x_n_hat(testing_sensitive, M_nk_sensitive, voptim, Ns, P, self.k)
-        x_n_hat_sensitive = res_sensitive[0]
-        res_nonsensitive = lfr_helpers.x_n_hat(testing_nonsensitive, M_nk_nonsensitive, voptim, N, P, self.k)
-        x_n_hat_nonsensitive = res_nonsensitive[0]
-
-        # compute predictions for test instances
-        res_sensitive = lfr_helpers.yhat(M_nk_sensitive, ytest_sensitive, woptim, Ns, self.k)
-        y_hat_sensitive = res_sensitive[0]
-        res_nonsensitive = lfr_helpers.yhat(M_nk_nonsensitive, ytest_nonsensitive, woptim, N, self.k)
-        y_hat_nonsensitive = res_nonsensitive[0]
+        _, features_hat_priveleged, labels_hat_priveleged = lfr_helpers.get_xhat_y_hat(self.prototypes, self.w, features_priveleged)
 
         transformed_features = np.zeros(shape=np.shape(dataset.features))
         transformed_labels = np.zeros(shape=np.shape(dataset.labels))
-        transformed_features[sensitive_idx] = x_n_hat_sensitive
-        transformed_features[nonsensitive_idx] = x_n_hat_nonsensitive
-        transformed_labels[sensitive_idx] = np.reshape(y_hat_sensitive,
-            [-1, 1])
-        transformed_labels[nonsensitive_idx] = np.reshape(y_hat_nonsensitive,
-            [-1, 1])
+        transformed_features[unprivileged_sample_ids] = features_hat_unpriveleged
+        transformed_features[privileged_sample_ids] = features_hat_priveleged
+        transformed_labels[unprivileged_sample_ids] = np.reshape(labels_hat_unpriveleged, [-1, 1])
+        transformed_labels[privileged_sample_ids] = np.reshape(labels_hat_priveleged,[-1, 1])
         transformed_bin_labels = (np.array(transformed_labels) > threshold).astype(np.float64)
 
         # Mutated, fairer dataset with new labels
@@ -180,3 +160,41 @@ class LFR(Transformer):
         """fit and transform methods sequentially"""
 
         return self.fit(dataset, seed=seed).transform(dataset)
+
+
+if __name__ == "__main__":
+
+    import sys
+    sys.path.append("../")
+
+    from aif360.metrics import BinaryLabelDatasetMetric
+    from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import load_preproc_data_adult
+
+    from sklearn.metrics import classification_report
+
+    dataset_orig = load_preproc_data_adult()
+    dataset_orig_train, dataset_orig_test = dataset_orig.split([0.7], shuffle=True)
+
+    privileged_groups = [{'sex': 1}]
+    unprivileged_groups = [{'sex': 0}]
+
+    TR = LFR(unprivileged_groups=unprivileged_groups, privileged_groups=privileged_groups, Ax=0.01, Ay=10.0, Az=1.0)
+    TR = TR.fit(dataset_orig_train, maxiter=5000)
+
+    metric_orig_train = BinaryLabelDatasetMetric(dataset_orig_train,
+                                                 unprivileged_groups=unprivileged_groups,
+                                                 privileged_groups=privileged_groups)
+    thresholds = [0.2, 0.4, 0.5, 0.6, 0.8]
+    for threshold in thresholds:
+        # Transform training data and align features
+        dataset_transf_train = TR.transform(dataset_orig_train, threshold=threshold)
+
+        metric_transf_train = BinaryLabelDatasetMetric(dataset_transf_train,
+                                                       unprivileged_groups=unprivileged_groups,
+                                                       privileged_groups=privileged_groups)
+        print("Classification threshold = %f" % threshold)
+        print(classification_report(dataset_orig_train.labels, dataset_transf_train.labels))
+        print(
+            "Difference in mean outcomes between unprivileged and privileged groups = %f" % metric_transf_train.mean_difference())
+        print("Consistency of labels in transformed training dataset= %f" % metric_transf_train.consistency())
+        print("Consistency of labels in original training dataset= %f" % metric_orig_train.consistency())
