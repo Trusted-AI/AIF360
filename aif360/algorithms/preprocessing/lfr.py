correct sign for softmax and other minor fixes.

aif360/algorithms/preprocessing/lfr.py
@@ -71,10 +71,12 @@ class LFR(Transformer):
         self.prototypes = None
         self.learned_model = None
 
-    def fit(self, dataset, maxiter=5000, maxfun=5000, **kwargs):
+    def fit(self, dataset, maxiter=5000, maxfun=5000):
         """Compute the transformation parameters that leads to fair representations.
         Args:
             dataset (BinaryLabelDataset): Dataset containing true labels.
+            maxiter (int): Maximum number of iterations.
+            maxfun (int): Maxinum number of function evaluations.
         Returns:
             LFR: Returns self.
         """
@@ -88,10 +90,10 @@ class LFR(Transformer):
             [-1, 1])
         unprivileged_sample_ids = np.array(np.where(protected_attributes == self.unprivileged_group_protected_attribute_value))[0].flatten()
         privileged_sample_ids = np.array(np.where(protected_attributes == self.privileged_group_protected_attribute_value))[0].flatten()
-        features_unpriveleged = dataset.features[unprivileged_sample_ids]
-        features_priveleged = dataset.features[privileged_sample_ids]
-        labels_unpriveleged = dataset.labels[unprivileged_sample_ids]
-        labels_priveleged = dataset.labels[privileged_sample_ids]
+        features_unprivileged = dataset.features[unprivileged_sample_ids]
+        features_privileged = dataset.features[privileged_sample_ids]
+        labels_unprivileged = dataset.labels[unprivileged_sample_ids]
+        labels_privileged = dataset.labels[privileged_sample_ids]
 
         # Initialize the LFR optim objective parameters
         parameters_initialization = np.random.uniform(size=self.k + self.features_dim * self.k)
@@ -104,9 +106,9 @@ class LFR(Transformer):
         lfr_helpers.LFR_optim_objective.steps = 0
 
         self.learned_model = optim.fmin_l_bfgs_b(lfr_helpers.LFR_optim_objective, x0=parameters_initialization, epsilon=1e-5,
-                                                      args=(features_unpriveleged, features_priveleged,
-                                        labels_unpriveleged[:, 0], labels_priveleged[:, 0], self.k, self.Ax,
-                                        self.Ay, self.Az, self.print_interval),
+                                                      args=(features_unprivileged, features_privileged,
+                                        labels_unprivileged[:, 0], labels_privileged[:, 0], self.k, self.Ax,
+                                        self.Ay, self.Az, self.print_interval, self.verbose),
                                                       bounds=bnd, approx_grad=True, maxfun=maxfun,
                                                       maxiter=maxiter, disp=self.verbose)[0]
         self.w = self.learned_model[:self.k]
@@ -114,7 +116,7 @@ class LFR(Transformer):
 
         return self
 
-    def transform(self, dataset, threshold=0.5, **kwargs):
+    def transform(self, dataset, threshold=0.5):
         """Transform the dataset using learned model parameters.
         Args:
             dataset (BinaryLabelDataset): Dataset containing labels that needs to be transformed.
@@ -132,21 +134,19 @@ class LFR(Transformer):
         np.array(np.where(protected_attributes == self.unprivileged_group_protected_attribute_value))[0].flatten()
         privileged_sample_ids = \
         np.array(np.where(protected_attributes == self.privileged_group_protected_attribute_value))[0].flatten()
-        features_unpriveleged = dataset.features[unprivileged_sample_ids]
-        features_priveleged = dataset.features[privileged_sample_ids]
-        labels_unpriveleged = dataset.labels[unprivileged_sample_ids]
-        labels_priveleged = dataset.labels[privileged_sample_ids]
+        features_unprivileged = dataset.features[unprivileged_sample_ids]
+        features_privileged = dataset.features[privileged_sample_ids]
 
-        _, features_hat_unpriveleged, labels_hat_unpriveleged = lfr_helpers.get_xhat_y_hat(self.prototypes, self.w, features_unpriveleged)
+        _, features_hat_unprivileged, labels_hat_unprivileged = lfr_helpers.get_xhat_y_hat(self.prototypes, self.w, features_unprivileged)
 
-        _, features_hat_priveleged, labels_hat_priveleged = lfr_helpers.get_xhat_y_hat(self.prototypes, self.w, features_priveleged)
+        _, features_hat_privileged, labels_hat_privileged = lfr_helpers.get_xhat_y_hat(self.prototypes, self.w, features_privileged)
 
         transformed_features = np.zeros(shape=np.shape(dataset.features))
         transformed_labels = np.zeros(shape=np.shape(dataset.labels))
-        transformed_features[unprivileged_sample_ids] = features_hat_unpriveleged
-        transformed_features[privileged_sample_ids] = features_hat_priveleged
-        transformed_labels[unprivileged_sample_ids] = np.reshape(labels_hat_unpriveleged, [-1, 1])
-        transformed_labels[privileged_sample_ids] = np.reshape(labels_hat_priveleged,[-1, 1])
+        transformed_features[unprivileged_sample_ids] = features_hat_unprivileged
+        transformed_features[privileged_sample_ids] = features_hat_privileged
+        transformed_labels[unprivileged_sample_ids] = np.reshape(labels_hat_unprivileged, [-1, 1])
+        transformed_labels[privileged_sample_ids] = np.reshape(labels_hat_privileged,[-1, 1])
         transformed_bin_labels = (np.array(transformed_labels) > threshold).astype(np.float64)
 
         # Mutated, fairer dataset with new labels
@@ -157,45 +157,16 @@ class LFR(Transformer):
 
         return dataset_new    
 
-    def fit_transform(self, dataset, seed=None):
-        """fit and transform methods sequentially"""
+    def fit_transform(self, dataset, maxiter=5000, maxfun=5000, threshold=0.5):
+        """Fit and transform methods sequentially.
 
-        return self.fit(dataset, seed=seed).transform(dataset)
+        Args:
+            dataset (BinaryLabelDataset): Dataset containing labels that needs to be transformed.
+            maxiter (int): Maximum number of iterations.
+            maxfun (int): Maxinum number of function evaluations.
+            threshold(float, optional): threshold parameter used for binary label prediction.
+        Returns:
+            dataset (BinaryLabelDataset): Transformed Dataset.
+        """
 
-
-if __name__ == "__main__":
-
-    import sys
-    sys.path.append("../")
-
-    from aif360.metrics import BinaryLabelDatasetMetric
-    from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import load_preproc_data_adult
-
-    from sklearn.metrics import classification_report
-
-    dataset_orig = load_preproc_data_adult()
-    dataset_orig_train, dataset_orig_test = dataset_orig.split([0.7], shuffle=True)
-
-    privileged_groups = [{'sex': 1}]
-    unprivileged_groups = [{'sex': 0}]
-
-    TR = LFR(unprivileged_groups=unprivileged_groups, privileged_groups=privileged_groups, Ax=0.01, Ay=10.0, Az=1.0)
-    TR = TR.fit(dataset_orig_train, maxiter=5000)
-
-    metric_orig_train = BinaryLabelDatasetMetric(dataset_orig_train,
-                                                 unprivileged_groups=unprivileged_groups,
-                                                 privileged_groups=privileged_groups)
-    thresholds = [0.2, 0.4, 0.5, 0.6, 0.8]
-    for threshold in thresholds:
-        # Transform training data and align features
-        dataset_transf_train = TR.transform(dataset_orig_train, threshold=threshold)
-
-        metric_transf_train = BinaryLabelDatasetMetric(dataset_transf_train,
-                                                       unprivileged_groups=unprivileged_groups,
-                                                       privileged_groups=privileged_groups)
-        print("Classification threshold = %f" % threshold)
-        print(classification_report(dataset_orig_train.labels, dataset_transf_train.labels))
-        print(
-            "Difference in mean outcomes between unprivileged and privileged groups = %f" % metric_transf_train.mean_difference())
-        print("Consistency of labels in transformed training dataset= %f" % metric_transf_train.consistency())
-        print("Consistency of labels in original training dataset= %f" % metric_orig_train.consistency())
+        return self.fit(dataset, maxiter=maxiter, maxfun=maxfun).transform(dataset, threshold=threshold)

aif360/algorithms/preprocessing/lfr_helpers/helpers.py
@@ -5,29 +5,29 @@ from scipy.spatial.distance import cdist
 from scipy.special import softmax
 
 
-def LFR_optim_objective(parameters, x_unpriveleged, x_priveleged, y_unpriveleged,
-                        y_priveleged, k=10, A_x=0.01, A_y=0.1, A_z=0.5, print_inteval=250):
+def LFR_optim_objective(parameters, x_unprivileged, x_privileged, y_unprivileged,
+                        y_privileged, k=10, A_x=0.01, A_y=0.1, A_z=0.5, print_interval=250, verbose=1):
 
-    num_unpriveleged, features_dim = x_unpriveleged.shape
-    num_priveleged, _ = x_priveleged.shape
+    num_unprivileged, features_dim = x_unprivileged.shape
+    num_privileged, _ = x_privileged.shape
 
     w = parameters[:k]
     prototypes = parameters[k:].reshape((k, features_dim))
 
-    M_unpriveleged, x_hat_unpriveleged, y_hat_unpriveleged = get_xhat_y_hat(prototypes, w, x_unpriveleged)
+    M_unprivileged, x_hat_unprivileged, y_hat_unprivileged = get_xhat_y_hat(prototypes, w, x_unprivileged)
 
-    M_priveleged, x_hat_priveleged, y_hat_priveleged = get_xhat_y_hat(prototypes, w, x_priveleged)
+    M_privileged, x_hat_privileged, y_hat_privileged = get_xhat_y_hat(prototypes, w, x_privileged)
 
-    y_hat = np.concatenate([y_hat_unpriveleged, y_hat_priveleged], axis=0)
-    y = np.concatenate([y_unpriveleged.reshape((-1, 1)), y_priveleged.reshape((-1, 1))], axis=0)
+    y_hat = np.concatenate([y_hat_unprivileged, y_hat_privileged], axis=0)
+    y = np.concatenate([y_unprivileged.reshape((-1, 1)), y_privileged.reshape((-1, 1))], axis=0)
 
-    L_x = np.mean((x_hat_unpriveleged - x_unpriveleged) ** 2) + np.mean((x_hat_priveleged - x_priveleged) ** 2)
-    L_z = np.mean(abs(np.mean(M_unpriveleged, axis=0) - np.mean(M_priveleged, axis=0)))
+    L_x = np.mean((x_hat_unprivileged - x_unprivileged) ** 2) + np.mean((x_hat_privileged - x_privileged) ** 2)
+    L_z = np.mean(abs(np.mean(M_unprivileged, axis=0) - np.mean(M_privileged, axis=0)))
     L_y = - np.mean(y * np.log(y_hat) + (1. - y) * np.log(1. - y_hat))
 
     total_loss = A_x * L_x + A_y * L_y + A_z * L_z
 
-    if LFR_optim_objective.steps % print_inteval == 0:
+    if verbose and LFR_optim_objective.steps % print_interval == 0:
         print("step: {}, loss: {}, L_x: {},  L_y: {},  L_z: {}".format(
             LFR_optim_objective.steps, total_loss, L_x,  L_y,  L_z))
     LFR_optim_objective.steps += 1
@@ -36,11 +36,11 @@ def LFR_optim_objective(parameters, x_unpriveleged, x_priveleged, y_unpriveleged
 
 
 def get_xhat_y_hat(prototypes, w, x):
-    M = softmax(cdist(x, prototypes), axis=1)
+    M = softmax(-cdist(x, prototypes), axis=1)
     x_hat = np.matmul(M, prototypes)
     y_hat = np.clip(
         np.matmul(M, w.reshape((-1, 1))),
-        1e-6,
-        0.999
+        np.finfo(float).eps,
+        1.0 - np.finfo(float).eps
     )
     return M, x_hat, y_hat