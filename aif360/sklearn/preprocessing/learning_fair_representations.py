351259b510398af0a2a56cf3c56db69e9b1014d2
Samuel Hoffman <hoffman.sc@gmail.com>
2020-07-31 16:07:39 -0400

    matches old lfr

--- aif360/sklearn/preprocessing/learning_fair_representations.py
+++ aif360/sklearn/preprocessing/learning_fair_representations.py
@@ -1,8 +1,12 @@
-from numba import njit
+import warnings
+
 import numpy as np
 import scipy.optimize as optim
-from scipy.special import softmax, logsumexp
+from scipy.spatial.distance import cdist
+from scipy.special import softmax
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.exceptions import ConvergenceWarning
+from sklearn.metrics import log_loss
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import check_random_state
 
@@ -34,7 +38,7 @@ class LearnedFairRepresentation(BaseEstimator, TransformerMixin):
 
     def __init__(self, prot_attr=None, n_prototypes=5,
                  reconstruction_weight=0.01, fairness_weight=50., epsilon=1e-8,
-                 max_iter=200, max_fun=5000, verbose=False, random_state=None):
+                 max_iter=200, max_fun=15000, verbose=0, random_state=None):
         """
         Args:
             prot_attr (single label or list-like, optional): Protected
@@ -53,16 +57,6 @@ class LearnedFairRepresentation(BaseEstimator, TransformerMixin):
         self.verbose = verbose
         self.random_state = random_state
 
-    # @staticmethod
-    # @njit
-    def _M(self, X, v, alpha):
-        return softmax(-np.dot((X[:, None, :] - v[None, :, :])**2, alpha), axis=1)
-        # # return softmax(-np.matmul((X[:, None, :] - v[None, :, :])**2, alpha), axis=1)
-        # m = -np.dot((X[:, None, :] - v[None, :, :])**2, alpha)
-        # # m = -(X[:, None, :] - v[None, :, :])**2 @ alpha
-        # # return np.exp(m - np.log(np.sum(np.exp(m), axis=1, keepdims=True)))
-        # return np.exp(m - logsumexp(m, axis=1, keepdims=True))
-
     def fit(self, X, y, priv_group=1, sample_weight=None):
         """Compute the transformation parameters that lead to fair
         representations.
@@ -76,6 +70,7 @@ class LearnedFairRepresentation(BaseEstimator, TransformerMixin):
         Returns:
             self
         """
+        # TODO: incorporate sample weight?
         X, y, sample_weight = check_inputs(X, y, sample_weight)
         rng = check_random_state(self.random_state)
 
@@ -87,93 +82,127 @@ class LearnedFairRepresentation(BaseEstimator, TransformerMixin):
         y = le.fit_transform(y)
         self.classes_ = le.classes_
         n_classes = len(self.classes_)
-        n_classes = 1  # XXX
+        if n_classes == 2:
+            n_classes = 1  # XXX
 
         n_prototypes = self.n_prototypes
 
         n_feat = X.shape[1]
-        x0 = rng.random(2*n_feat + n_prototypes*(n_classes + n_feat))
-        # bounds = ([(None, None)]*n_feat*2 + [(1e-5, 1-1e-5)]*n_prototypes*n_classes
-        bounds = ([(None, None)]*n_feat*2 + [(0, 1)]*n_prototypes*n_classes
+        x0 = rng.random(n_prototypes*(n_classes + n_feat))
+        bounds = ([(0, 1)]*n_prototypes*n_classes
                 + [(None, None)]*n_prototypes*n_feat)
 
-        # @njit
-        def unpack(x):
-            alpha = x[:2*n_feat].reshape((2, -1))
-            w = x[2*n_feat:2*n_feat + n_prototypes*n_classes]#.reshape(-1, n_classes)
-            v = x[2*n_feat + n_prototypes*n_classes:].reshape(-1, n_feat)
-            return alpha, w, v
+        step = 0
+        def LFR_optim_objective(x, X_plus, X_minus, y_plus, y_minus):
+            nonlocal step
+            w = x[:n_prototypes*n_classes].reshape(-1, n_classes)
+            v = x[n_prototypes*n_classes:].reshape(-1, n_feat)
+            M_plus = softmax(-cdist(X_plus, v), axis=1)
+            M_minus = softmax(-cdist(X_minus, v), axis=1)
+            yhat = np.concatenate((M_plus.dot(w), M_minus.dot(w)), axis=0)
+            if n_classes > 1:
+                yhat = softmax(yhat, axis=1)
+            y = np.concatenate((y_plus, y_minus), axis=0).reshape(-1, 1)
 
-        # @njit
-        # def _M(X, v, alpha):
-        #     # m = -np.dot((X[:, None, :] - v[None, :, :])**2, alpha)
-        #     # print(X.shape, v.shape, alpha.shape)
-        #     # m = -np.dot((np.expand_dims(X, 1) - np.expand_dims(v, 0))**2, alpha)
-        #     m = -np.matmul((np.expand_dims(X, 1) - np.expand_dims(v, 0))**2, alpha)
-        #     return np.exp(m - np.log(np.sum(np.exp(m), axis=1, keepdims=True)))
+            L_x = np.mean((X_plus - M_plus.dot(v))**2) \
+                + np.mean((X_minus - M_minus.dot(v))**2)
+            L_y = log_loss(y, yhat, eps=np.finfo(w.dtype).eps)
+            L_z = np.mean(np.abs(M_plus.mean(axis=0) - M_minus.mean(axis=0)))
+            L = self.reconstruction_weight*L_x + L_y + self.fairness_weight*L_z
 
-        # @njit
-        def LFR_optim_obj(x, X_plus, X_minus, y_plus, y_minus, A_x, A_z):
-            alpha, w, v = unpack(x)
-            M_plus = self._M(X_plus, v, alpha[1])
-            M_minus = self._M(X_minus, v, alpha[0])
-            eps = 0  # np.finfo(w.dtype).eps
+            if self.verbose > 0 and step % self.verbose == 0:
+                print("step: {:{}d}, loss: {:10.3f}, Ax*Lx: {:10.3f}, Ly: {:10.3f}, Az*Lz: {:10.3f}".format(
+                      step, int(np.log10(self.max_fun)+1), L, self.reconstruction_weight*L_x, L_y, self.fairness_weight*L_z))
+            step += 1
+            return L
 
-            L_x = np.sum((X_plus - M_plus.dot(v))**2) \
-                + np.sum((X_minus - M_minus.dot(v))**2)
-            L_y1 = np.sum(-y_plus*np.log(M_plus.dot(w) + eps)
-                    - (1 - y_plus)*np.log(1 - M_plus.dot(w) + eps))
-            L_y2 = np.sum(-y_minus*np.log(M_minus.dot(w) + eps)
-                    - (1 - y_minus)*np.log(1 - M_minus.dot(w) + eps))
-            L_z = np.abs(M_plus.mean(axis=0) - M_minus.mean(axis=0)).sum()
-            return A_x*L_x + L_y1 + L_y2 + A_z*L_z
-
-        # x_min, _, d = optim.fmin_l_bfgs_b(
-        #         LFR_optim_obj, x0=x0, epsilon=self.epsilon,
+        # res = optim.minimize(LFR_optim_objective, x0=x0,
         #         args=(X[priv].to_numpy(), X[~priv].to_numpy(), y[priv], y[~priv]),
-        #         bounds=bounds, approx_grad=True, maxfun=self.max_fun,
-        #         maxiter=self.max_iter, disp=50 if self.verbose else 0)
-        res = optim.minimize(LFR_optim_obj, x0=x0,
-                args=(X[priv].to_numpy(), X[~priv].to_numpy(), y[priv], y[~priv], self.reconstruction_weight, self.fairness_weight),
-                method='L-BFGS-B', bounds=bounds, options=dict(
-                eps=self.epsilon, maxiter=self.max_iter,
-                maxfun=self.max_fun, disp=50 if self.verbose else 0))
-        self.alpha_, self.coef_, self.prototypes_ = unpack(res.x)
-        self.n_iter_ = res.nit  # d['nit']
-        self.n_fun_ = res.nfev  # d['funcalls']
+        #         method='L-BFGS-B', bounds=bounds, options={'gtol': tol, 'maxiter': self.max_iter}))
+        #         dict(eps=self.epsilon, maxiter=self.max_iter,
+        #         maxfun=self.max_fun, disp=50 if self.verbose else 0))
+        # self.n_iter_ = res.nit  # d['nit']
+        # self.n_fun_ = res.nfev  # d['funcalls']
+
+        x_min, _, d = optim.fmin_l_bfgs_b(
+                LFR_optim_objective, x0=x0, epsilon=self.epsilon,
+                args=(X[priv].to_numpy(), X[~priv].to_numpy(), y[priv], y[~priv]),
+                bounds=bounds, approx_grad=True, maxfun=self.max_fun,
+                maxiter=self.max_iter)
+        self.coef_ = x_min[:n_prototypes*n_classes].reshape(-1, n_classes)
+        self.prototypes_ = x_min[n_prototypes*n_classes:].reshape(-1, n_feat)
+        self.n_iter_ = d['nit']
+        self.n_fun_ = d['funcalls']
+        if d['warnflag'] == 0 and self.verbose:
+            print("Converged!")
+        elif d['warnflag'] == 1 and self.n_fun_ >= self.max_fun:
+            warnings.warn("lbfgs failed to converge. Increase the number of function evaluations.",
+                          ConvergenceWarning)
+        elif d['warnflag'] == 1 and self.n_iter_ >= self.max_iter:
+            warnings.warn("lbfgs failed to converge. Increase the number of iterations.",
+                          ConvergenceWarning)
+        else:
+            warnings.warn("lbfgs failed to converge: {}".format(d['task'].decode()), ConvergenceWarning)
 
         return self
 
-    def transform(self, X, y):
+    def transform(self, X):
         """Transform the dataset using the learned model parameters.
 
         Args:
-            X (pandas.DataFrame): Test samples.
-            y (pandas.Series or pandas.DataFrame): Test targets.
+            X (pandas.DataFrame): Training samples.
 
         Returns:
-            tuple:
-                Transformed samples and targets.
-
-                * **Xt** -- Transformed samples.
-                * **yt** -- Transformed targets.
+            pandas.DataFrame: Transformed samples.
         """
         groups, _ = check_groups(X, self.prot_attr_)
         priv = (groups == self.priv_group_)
 
-        M_plus = self._M(X[priv].to_numpy(), self.prototypes_, self.alpha_[1])
-        M_minus = self._M(X[~priv].to_numpy(), self.prototypes_, self.alpha_[0])
+        M_plus = softmax(-cdist(X[priv].to_numpy(), self.prototypes_), axis=1)
+        M_minus = softmax(-cdist(X[~priv].to_numpy(), self.prototypes_), axis=1)
 
-        Xt = X.copy()
-        yt = y.copy()
+        Xt = X.copy()  # TODO: avoid copy
         Xt[priv] = M_plus.dot(self.prototypes_)
         Xt[~priv] = M_minus.dot(self.prototypes_)
+
+        return Xt
+
+    def predict_proba(self, X):
+        """Transform the targets using the learned model parameters.
+
+        Args:
+            X (pandas.DataFrame): Training samples.
+
+        Returns:
+            numpy.ndarray: Transformed targets. Returns the probability of the
+            sample for each class in the model, where classes are ordered as
+            they are in ``self.classes_``.
+        """
+        groups, _ = check_groups(X, self.prot_attr_)
+        priv = (groups == self.priv_group_)
+
+        M_plus = softmax(-cdist(X[priv].to_numpy(), self.prototypes_), axis=1)
+        M_minus = softmax(-cdist(X[~priv].to_numpy(), self.prototypes_), axis=1)
+
+        yt = np.empty((X.shape[0], self.coef_.shape[1]))  # TODO: dtype?
         yt[priv] = M_plus.dot(self.coef_)
         yt[~priv] = M_minus.dot(self.coef_)
-        # yt = self.classes_[yt.argmax(axis=1)]
-        yt = self.classes_[(yt > 0.5).astype(int)]
+        if yt.shape[1] == 1:
+            yt = np.c_[1-yt, yt]
+        else:
+            yt = softmax(yt, axis=1)
+        return yt
 
-        return Xt, yt
+    def predict(self, X):
+        """Transform the targets using the learned model parameters.
 
-    def fit_transform(self, X, y, **fit_params):
-        return self.fit(X, y, **fit_params).transform(X, y)
+        Args:
+            X (pandas.DataFrame): Training samples.
+
+        Returns:
+            numpy.ndarray: Transformed targets.
+        """
+        probas = self.predict_proba(X)
+        return self.classes_[probas.argmax(axis=1)]
+
+    # TODO: fit_transform_predict()??
--- tests/sklearn/test_learning_fair_representations.py
+++ tests/sklearn/test_learning_fair_representations.py
@@ -1,103 +1,96 @@
 import numpy as np
-np.set_printoptions(linewidth=200)
+# np.set_printoptions(linewidth=200)
 import pandas as pd
-pd.set_option('display.width', 200)
-pd.set_option('max_columns', 10)
-from time import time
+# pd.set_option('display.width', 200)
+# pd.set_option('max_columns', 10)
 import pytest
-# from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline
 
-from aif360.datasets import AdultDataset
-from aif360.sklearn.datasets import fetch_adult
+from aif360.datasets import GermanDataset  # AdultDataset
+from aif360.sklearn.datasets import fetch_german  # fetch_adult
 from aif360.algorithms.preprocessing import LFR
 from aif360.sklearn.preprocessing import LearnedFairRepresentation
+from aif360.sklearn.metrics import make_scorer, statistical_parity_difference
 
 
 @pytest.fixture(scope='module')
-def old_adult():
-    return AdultDataset(instance_weights_name='fnlwgt', categorical_features=[],
-            features_to_keep=['age', 'education-num', 'capital-gain',
-                              'capital-loss', 'hours-per-week'],
-            features_to_drop=[])
+def old_german():
+    german = GermanDataset(categorical_features=['foreign_worker'],
+            features_to_keep=['month', 'credit_amount',
+            'investment_as_income_percentage', 'residence_since', 'age',
+            'number_of_credits', 'people_liable_for', 'sex'])
+    german.features = np.concatenate(
+            (german.features[:, :-3], german.features[:, -2:-4:-1]), axis=1)
+    german.feature_names = german.feature_names[:-3] + ['foreign_worker', 'sex']
+    german.labels[german.labels == 2] = 0
+    return german
 
 @pytest.fixture(scope='module')
-def old_lfr(old_adult):
-    lfr = LFR(unprivileged_groups=[{'sex': 0}],
-              privileged_groups=[{'sex': 1}], verbose=0, seed=123)
-    return lfr.fit_transform(old_adult)
+def old_lfr(old_german):
+    lfr = LFR(unprivileged_groups=[{'age': 0}], privileged_groups=[{'age': 1}],
+              seed=123)
+    return lfr.fit(old_german, maxfun=3e4)
 
 @pytest.fixture(scope='module')
-def old_lfr2(old_adult):
-    lfr = LFR(unprivileged_groups=[{'sex': 0}],
-              privileged_groups=[{'sex': 1}], verbose=0, seed=123)
-    return lfr.fit_transform(old_adult)
+def old_lfr2(old_german):
+    lfr = LFR(unprivileged_groups=[{'age': 0}], privileged_groups=[{'age': 1}],
+              seed=123)
+    return lfr.fit_transform(old_german, maxfun=3e4)
 
 @pytest.fixture(scope='module')
-def new_adult():
-    return fetch_adult(numeric_only=True)
+def new_german():
+    german = fetch_german(numeric_only=True)
+    german.X.age = german.X.age.apply(lambda a: 1 if a > 25 else 0)
+    return german
 
 @pytest.fixture(scope='module')
-def new_lfr(new_adult):
-    lfr = LearnedFairRepresentation('sex', random_state=123, epsilon=1e-5)
-    X, y, sample_weight = new_adult
-    # return lfr.fit_transform(X, y, sample_weight=sample_weight)
-    Xt, yt = lfr.fit_transform(X, y, sample_weight=sample_weight)
-    print(f'n_iter: {lfr.n_iter_}')
-    print(f'n_fun: {lfr.n_fun_}')
-    return Xt, yt
+def new_lfr(new_german):
+    lfr = LearnedFairRepresentation('age', epsilon=1e-5, max_fun=3e4, random_state=123)
+    return lfr.fit(**new_german._asdict())
 
 @pytest.fixture
-def new_lfr2(new_adult):
-    lfr = LearnedFairRepresentation('sex', random_state=123, epsilon=1e-5)
-    X, y, sample_weight = new_adult
-    return lfr.fit_transform(X, y, sample_weight=sample_weight)
+def new_lfr2(new_german):
+    lfr = LearnedFairRepresentation('age', epsilon=1e-5, max_fun=3e4, random_state=123)
+    X, y = new_german
+    Xt = lfr.fit_transform(X, y)
+    yt = lfr.predict(X)
+    return Xt, yt
 
 
-def test_lfr_old_reproduce(old_lfr, old_lfr2):
+def test_data_old_new(old_german, new_german):
+    """Test that the old and new versions of the dataset match."""
+    assert (old_german.features == new_german.X).all(None)
+    assert (old_german.labels.flatten() == new_german.y).all()
+
+def test_lfr_old_reproduce(old_lfr, old_lfr2, old_german):
     """Test that the old LFR is reproducible."""
+    old_lfr = old_lfr.transform(old_german)
     assert np.allclose(old_lfr.features, old_lfr2.features)
     assert np.allclose(old_lfr.labels, old_lfr2.labels)
 
-def test_lfr_reproduce(new_lfr, new_lfr2):
+def test_lfr_new_reproduce(new_lfr, new_lfr2, new_german):
     """Test that the new LearnedFairRepresentation is reproducible."""
-    Xt, yt = new_lfr
+    Xt = new_lfr.transform(new_german.X)
+    yt = new_lfr.predict(new_german.X)
     Xt2, yt2 = new_lfr2
 
     assert np.allclose(Xt, Xt2)
     assert np.allclose(yt, yt2)
 
-def test_lfr_old_new(old_lfr, new_lfr):
+def test_lfr_old_new(old_lfr2, new_lfr2):
     """Test that the transformations of the old and new LFR match."""
-    print()
-    print(old_lfr.features[:10])
-    print(new_lfr[0][:10])
-    assert np.allclose(old_lfr.features, new_lfr[0])
-    assert np.allclose(old_lfr.labels.flatten(), new_lfr[1])
+    Xt, yt = new_lfr2
+    assert np.allclose(old_lfr2.features, Xt)
+    assert np.allclose(old_lfr2.labels.flatten(), yt)
 
-def test_models(old_adult, new_adult):
-    oldlfr = LFR(unprivileged_groups=[{'sex': 0}],
-              privileged_groups=[{'sex': 1}], verbose=0, seed=123)
-    oldlfr.fit(old_adult)
-
-    newlfr = LearnedFairRepresentation('sex', random_state=123)
-    X, y, sample_weight = new_adult
-    newlfr.fit(X, y, sample_weight=sample_weight)
-    new_model = np.concatenate((newlfr.alpha_.flatten(), newlfr.coef_.flatten(), newlfr.prototypes_.flatten()))
-
-    # print(oldlfr.learned_model)
-    # print(new_model)
-    print()
-    print(newlfr.prototypes_)
-    print(oldlfr.learned_model[19:].reshape(5, -1))
-
-    assert np.allclose(oldlfr.learned_model, new_model)
-    # print(newlfr.alpha_[0])
-    # print(oldlfr.learned_model[:7])
-    # print(newlfr.alpha_[1])
-    # print(oldlfr.learned_model[7:14])
-    # print(newlfr.coef_)
-    # print(oldlfr.learned_model[14:19])
+def test_lfr_models(old_lfr, new_lfr):
+    """Test that the learned model parameters of the old and new LFR match."""
+    print(new_lfr.n_iter_, new_lfr.n_fun_)
+    assert np.allclose(old_lfr.w, new_lfr.coef_.flatten())
+    assert np.allclose(old_lfr.prototypes, new_lfr.prototypes_)
 
 
 # def test_lfr_multilabel():
@@ -106,15 +99,24 @@ def test_models(old_adult, new_adult):
 #     lfr.fit(X, y)
 #     assert lfr.coef_.shape[1] == 4
 
-# def test_lfr_grid():
-#     """Test that the new LearnedFairRepresentation works in a grid search (and that
-#     debiasing results in reduced accuracy).
-#     """
-#     lfr = LearnedFairRepresentation('sex', random_state=123)
+def test_lfr_grid(new_german):
+    """Test that the new LFR works in a grid search (and that debiasing
+    results in improved statistical parity difference).
+    """
+    X, y = new_german
+    lfr = LearnedFairRepresentation('age', random_state=123)
+    params = {'fairness_weight': [50, 0]}
+    min_disc = make_scorer(statistical_parity_difference, prot_attr='age')
+    clf = GridSearchCV(lfr, params, scoring=min_disc, cv=2)
+    clf.fit(X, y)
 
-#     params = {'debias': [True, False]}
+    assert clf.best_params_ == {'fairness_weight': 50}
 
-#     clf = GridSearchCV(lfr, params, cv=3)
-#     clf.fit(X, y)
+def test_lfr_pipe(new_german):
+    """Test that the new LFR works as a pre-processing step in a pipeline."""
+    X, y = new_german
+    lfr = LearnedFairRepresentation('age', random_state=123)
+    lr = LogisticRegression(solver='lbfgs', random_state=123)
+    pipe = make_pipeline(lfr, lr)
 
-#     assert clf.best_params_ == {'debias': False}
+    pipe.fit(X, y)
