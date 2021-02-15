ccc0dd5499c4da885362c0cf46d30545228b5c0d
Samuel Hoffman <hoffman.sc@gmail.com>
2021-02-15 12:40:59 -0800

    self.model.constraints -> self.model.moment
    Signed-off-by: Samuel Hoffman <hoffman.sc@gmail.com>

--- aif360/algorithms/inprocessing/grid_search_reduction.py
+++ aif360/algorithms/inprocessing/grid_search_reduction.py
@@ -134,7 +134,7 @@ class GridSearchReduction(Transformer):
         dataset_new = dataset.copy()
         dataset_new.labels = self.model.predict(X_df).reshape(-1, 1)
 
-        if isinstance(self.model.constraints, red.ClassificationMoment):
+        if isinstance(self.model.moment, red.ClassificationMoment):
             fav = int(dataset.favorable_label)
             try:
                 # Probability of favorable label
