d4dd16cdfafee098f9764fe272f3a803e94cf4c8
Victor Akinwande <victor.akinwande1@ibm.com>
2021-04-20 10:55:12 -0500

    Implements multidimensional subset scanning and uses it to identify bias in classifiers. Includes Bernoulli, Poisson and Berk Jones scoring functions, and example notebooks.
    Signed-off-by: Victor Akinwande <victor.akinwande1@ibm.com>

---
+++ aif360/metrics/mdss/ScoringFunctions/__init__.py
@@ -0,0 +1,4 @@
+from aif360.metrics.mdss.ScoringFunctions.ScoringFunction import ScoringFunction
+from aif360.metrics.mdss.ScoringFunctions.Bernoulli import Bernoulli
+from aif360.metrics.mdss.ScoringFunctions.Poisson import Poisson
+from aif360.metrics.mdss.ScoringFunctions.BerkJones import BerkJones
---
+++ tests/test_demo_mdss_classifier_metric.py
@@ -0,0 +1,14 @@
+from notebook_runner import notebook_run
+import os
+
+def test_demo_mdss_classifier_metric():
+    nb, errors = notebook_run(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '..','examples', 
+            'demo_mdss_classifier_metric.ipynb'))
+
+    if len(errors) > 0:
+        for err in errors:
+            for tbi in err['traceback']:
+                print(tbi)
+        raise AssertionError("errors in notebook testcases")
