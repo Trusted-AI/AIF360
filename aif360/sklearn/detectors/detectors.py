eed82c17fd6f81375af8d3f56a182338f84c83de
Adebayo Oshingbesan <Adebayo.Oshingbesan1@ibm.com>
2022-04-23 01:33:16 +0300

    Improve mdss detector api
    Signed-off-by: Adebayo Oshingbesan <Adebayo.Oshingbesan1@ibm.com>

--- aif360/sklearn/detectors/detectors.py
+++ aif360/sklearn/detectors/detectors.py
@@ -5,25 +5,33 @@ from aif360.detectors.mdss import ScoringFunction
 
 import pandas as pd
 
-def mdss_bias_scan(
+
+def bias_scan(
     data: pd.DataFrame,
     observations: pd.Series,
-    expectations: pd.Series = None,
+    expectations: Union[pd.Series, pd.DataFrame] = None,
+    favorable_value: float = None,
     overpredicted: bool = True,
     scoring: Union[str, ScoringFunction] = "Bernoulli",
     num_iters: int = 10,
     penalty: float = 1e-17,
-    pos_label: int = None,
-    **kwargs
+    mode: str = "binary",
+    **kwargs,
 ):
     """
-    scan to find the highest scoring subset of records. For example usage see demo_mdss_detector.ipynb under examples
+    scan to find the highest scoring subset of records (see demo_mdss_detector.ipynb for example usage)
 
     :param data (dataframe): the dataset (containing the features) the model was trained on
     :param observations (series): ground truth (correct) target values
-    :param expectations (series, optional): estimated targets as returned by a model.
-        If None, model is assumed to be a dumb model that predicts the mean of the targets.
-        If pos_label is set, expectations is the model's predicted probabilities of the positive label.
+    :param expectations (series,  dataframe, optional): pandas series estimated targets
+        as returned by a model for binary, continuous and ordinal modes.
+        If mode is nominal, this is a dataframe with columns containing expectations for each nominal class.
+        If None, model is assumed to be a dumb model that predicts the mean of the targets
+                or 1/(num of categories) for nominal mode.
+    :param favorable_value(float, optional): Should be either the max or min value in the observations
+            if mode is one of binary, ordinal, or continuous. Defaults to max if None for these modes.
+            If mode is nominal, favorable label should be one of the unique categories in the observations.
+            Defaults to a one-vs-all scan if None for nominal mode.
     :param overpredicted (bool, optional): flag for group to scan for.
         True means we scan for a group whose expectations/predictions are systematically higher than observed.
         In other words, True means we scan for a group whose observeed is systematically lower than the expectations.
@@ -35,8 +43,9 @@ def mdss_bias_scan(
     :param penalty (float,optional): penalty term. Should be positive. The penalty term as with any regularization parameter may need to be
         tuned for ones use case. The higher the penalty, the less complex (number of features and feature values) the
         highest scoring subset that gets returned is.
-    :param pos_label(int, optional): positive label in the case of a binary classification task. Should be 1 or 0.
-        If binary classification task but positive_label not set, pos_label is assumed to be 1.
+    :param mode: one of ['binary', 'continuous', 'nominal', 'ordinal']. Defaults to binary.
+            In nominal mode, up to 10 categories are supported by default.
+            To increase this, pass in keyword argument max_nominal = integer value.
 
     :returns: the highest scoring subset and the score
     """
@@ -44,10 +53,10 @@ def mdss_bias_scan(
         data=data,
         observations=observations,
         expectations=expectations,
+        favorable_value=favorable_value,
         overpredicted=overpredicted,
         scoring=scoring,
         num_iters=num_iters,
         penalty=penalty,
-        pos_label=pos_label,
-        kwargs=kwargs,
+        mode=mode,
     )
