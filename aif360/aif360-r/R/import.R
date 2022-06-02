f05295a3b46208f444280bc9e8897b9e2d01d21c
milevavantuyl <mvantuyl@wellesley.edu>
2022-06-02 13:05:07 -0700

    Added error message to address locked binding
    Signed-off-by: milevavantuyl <mvantuyl@wellesley.edu>

--- aif360/aif360-r/R/import.R
+++ aif360/aif360-r/R/import.R
@@ -3,16 +3,21 @@
 #'
 load_aif360_lib <- function() {
   e <- globalenv()
-  e$datasets <- import("aif360.datasets")
-  e$metrics <- import("aif360.metrics")
-  e$pre_algo <- import("aif360.algorithms.preprocessing")
-  e$in_algo <- import("aif360.algorithms.inprocessing")
-  e$post_algo <- import("aif360.algorithms.postprocessing")
-  e$tf <- import("tensorflow")
-  lockBinding("datasets", e)
-  lockBinding("metrics", e)
-  lockBinding("pre_algo", e)
-  lockBinding("in_algo", e)
-  lockBinding("post_algo", e)
-  lockBinding("tf", e)
+  bindings <- c("datasets", "metrics", "pre_algo", "in_algo", "post_algo", "tf")
+  if (!all(bindings %in% ls(e))){
+    e$datasets <- import("aif360.datasets")
+    e$metrics <- import("aif360.metrics")
+    e$pre_algo <- import("aif360.algorithms.preprocessing")
+    e$in_algo <- import("aif360.algorithms.inprocessing")
+    e$post_algo <- import("aif360.algorithms.postprocessing")
+    e$tf <- import("tensorflow")
+    lockBinding("datasets", e)
+    lockBinding("metrics", e)
+    lockBinding("pre_algo", e)
+    lockBinding("in_algo", e)
+    lockBinding("post_algo", e)
+    lockBinding("tf", e)
+  } else {
+    message(paste0(c("The aif360 functions have already been loaded:", bindings), sep = " "))
+  }
 }
