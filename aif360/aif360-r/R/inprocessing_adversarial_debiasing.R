32b7142c5b450e52d53b248df6b8f2560338bce1
Josue Rodriguez <josue.rodriguez594@gmail.com>
2022-06-28 09:39:17 -0700

    tidy inprocessing_adversarial_debiasing documentation
    Signed-off-by: Josue Rodriguez <josue.rodriguez594@gmail.com>

--- aif360/aif360-r/R/inprocessing_adversarial_debiasing.R
+++ aif360/aif360-r/R/inprocessing_adversarial_debiasing.R
@@ -1,15 +1,16 @@
 #' Adversarial Debiasing
-#' @description Adversarial debiasing is an in-processing technique that learns a classifier to maximize prediction accuracy and simultaneously reduce an adversary's ability to determine the protected attribute from the predictions
-#' @param unprivileged_groups a list with two values: the column of the protected class and the value indicating representation for unprivileged group
-#' @param privileged_groups a list with two values: the column of the protected class and the value indicating representation for privileged group
-#' @param scope_name scope name for the tensorflow variables
+#' @description Adversarial debiasing is an in-processing technique that learns a classifier to maximize prediction accuracy 
+#' and simultaneously reduce an adversary's ability to determine the protected attribute from the predictions
+#' @param unprivileged_groups A list with two values: the column of the protected class and the value indicating representation for unprivileged group.
+#' @param privileged_groups A list with two values: the column of the protected class and the value indicating representation for privileged group.
+#' @param scope_name Scope name for the tensorflow variables.
 #' @param sess tensorflow session
-#' @param seed seed to make `predict` repeatable.
-#' @param adversary_loss_weight hyperparameter that chooses the strength of the adversarial loss.
-#' @param num_epochs number of training epochs.
-#' @param batch_size batch size.
-#' @param classifier_num_hidden_units number of hidden units in the classifier model.
-#' @param debias learn a classifier with or without debiasing.
+#' @param seed Seed to make \code{predict} repeatable. If not, \code{NULL}, must be an integer.
+#' @param adversary_loss_weight Hyperparameter that chooses the strength of the adversarial loss.
+#' @param num_epochs Number of training epochs. Must be an integer.
+#' @param batch_size Batch size. Must be an integer.
+#' @param classifier_num_hidden_units Number of hidden units in the classifier model. Must be an integer.
+#' @param debias Learn a classifier with or without debiasing.
 #' @examples
 #' \dontrun{
 #' load_aif360_lib()
@@ -17,22 +18,29 @@
 #' p <- list("race", 1)
 #' u <- list("race", 0)
 #'
-#' sess = tf$compat$v1$Session()
+#' sess <- tf$compat$v1$Session()
 #'
-#' plain_model = adversarial_debiasing(privileged_groups = p,
-#'                                     unprivileged_groups = u,
-#'                                     scope_name='debiased_classifier',
-#'                                     debias=TRUE,
-#'                                     sess=sess)
+#' plain_model <- adversarial_debiasing(privileged_groups = p,
+#'                                      unprivileged_groups = u,
+#'                                      scope_name = "debiased_classifier",
+#'                                      debias = TRUE,
+#'                                      sess = sess)
 #'
 #' plain_model$fit(ad)
 #' ad_nodebiasing <- plain_model$predict(ad)
 #' }
 #' @export
 #'
-adversarial_debiasing <- function(unprivileged_groups, privileged_groups, scope_name='current', sess=tf$compat$v1$Session(),
-                                  seed=NULL, adversary_loss_weight=0.1, num_epochs=50L, batch_size=128L,
-                                  classifier_num_hidden_units=200L, debias=TRUE) {
+adversarial_debiasing <- function(unprivileged_groups, 
+                                  privileged_groups, 
+                                  scope_name = "current", 
+                                  sess = tf$compat$v1$Session(),
+                                  seed = NULL, 
+                                  adversary_loss_weight = 0.1, 
+                                  num_epochs = 50L, 
+                                  batch_size = 128L,
+                                  classifier_num_hidden_units = 200L, 
+                                  debias = TRUE) {
   
   
   
