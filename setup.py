3ead939920410bc0172b745fb951f96b471d220d
Andrés Ignacio Torres <andresitorresm@gmail.com>
2022-09-21 16:13:46 -0400

    Add code coverage checks with `pytest-cov` (#412)
    Added `pytest-cov>=2.8.1` since `2.8.1` is the last version that supports `pytest` `3.5.0`, to avoid breaking compatibility.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras = {
     'notebooks': ['jupyter', 'tqdm', 'igraph[plotting]', 'lightgbm', 'seaborn', 'ipympl'],
     'LawSchoolGPA': ['tempeh'],
 }
-extras['tests'] = reduce(lambda l1, l2: l1+l2, extras.values(), ['pytest>=3.5'])
+extras['tests'] = reduce(lambda l1, l2: l1+l2, extras.values(), ['pytest>=3.5', 'pytest-cov>=2.8.1'])
 extras['docs'] = ['sphinx<2', 'jinja2<3.1.0', 'sphinx_rtd_theme']
 extras['all'] = list(reduce(lambda s, l: s.union(l), extras.values(), set()))
 
