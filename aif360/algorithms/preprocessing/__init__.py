de393cc815d64013a246113ef19bf287f3f4f12b
GIordano <giordano.daloisio@gmail.com>
2023-02-13 15:26:26 +0100

    added demv
    Signed-off-by: GIordano <giordano.daloisio@gmail.com>

--- aif360/algorithms/preprocessing/__init__.py
+++ aif360/algorithms/preprocessing/__init__.py
@@ -2,3 +2,4 @@ from aif360.algorithms.preprocessing.disparate_impact_remover import DisparateIm
 from aif360.algorithms.preprocessing.lfr import LFR
 from aif360.algorithms.preprocessing.optim_preproc import OptimPreproc
 from aif360.algorithms.preprocessing.reweighing import Reweighing
+from aif360.algorithms.preprocessing.demv import DEMV
