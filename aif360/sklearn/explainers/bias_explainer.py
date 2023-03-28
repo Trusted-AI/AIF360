974bd6eadaa343effb57dd07b596005fcba56d11
bishwamittra <bishwa12buet@gmail.com>
2023-03-28 13:20:33 +0200

    compatible for three datasets
    Signed-off-by: bishwamittra <bishwa12buet@gmail.com>

--- aif360/sklearn/explainers/bias_explainer.py
+++ aif360/sklearn/explainers/bias_explainer.py
@@ -1,5 +1,8 @@
+import sys
+sys.path.append("../../../script_fairXplainer_public/")
 from fairxplainer.fair_explainer import FairXplainer
 from fairxplainer.fair_explainer import plot as fif_plot
+import numpy as np
 
 
 def explain_statistical_parity(
@@ -7,13 +10,13 @@ def explain_statistical_parity(
         X, 
         sensitive_features, 
         maxorder=2, 
-        spline_intervals=6,
-        draw_plot=False,
+        spline_intervals=5,
+        k=7,
         seed=22, 
         cpu_time=300, 
         verbose=False):
     """
-        Explain the bias of a model given a dataset
+        Explain the statistical parity of a classifier given a dataset
 
     Args:
         clf: the trained model to explain
@@ -22,7 +25,7 @@ def explain_statistical_parity(
         sensitive_features (list): list of sensitive attributes
         maxorder (int): maximum order of the intersectionality of features
         spline_intervals (int): number of intervals to use for the cubic spline approximation
-        draw_plot (bool): whether to draw the plot
+        k (int): number of top feature interactions to show
         seed (int): seed for the random number generator
         cpu_time (int): maximum time to run the algorithm
         verbose (bool): whether to print the results
@@ -45,16 +48,148 @@ def explain_statistical_parity(
             cpu_time=cpu_time, 
             verbose=verbose)
     
-    result = fairXplainer.get_top_k_weights()
+    result = fairXplainer.get_top_k_weights(k=k)
 
-    if draw_plot:
-        plt = fif_plot(result, 
-               draw_waterfall=True, 
-               figure_size=(6.5,4), 
+    if(verbose):
+        print("Exact statistical parity", fairXplainer.statistical_parity_sample())
+
+    return result, fairXplainer.statistical_parity_sample()
+
+def explain_equalized_odds(
+        clf,
+        X,
+        y_true,
+        sensitive_features,
+        maxorder=2,
+        spline_intervals=5,
+        k=7,
+        seed=22,
+        cpu_time=300,
+        verbose=False):
+    
+    """
+        Explain the equalized odds of a classifier given a dataset
+
+    Args:
+        clf: the trained model to explain
+            clf.predict() should be implemented
+        X (pandas.DataFrame): the dataset (containing the features) to measure the bias on
+        y_true (pandas.Series): the labels of the dataset
+        sensitive_features (list): list of sensitive attributes
+        maxorder (int): maximum order of the intersectionality of features
+        spline_intervals (int): number of intervals to use for the cubic spline approximation
+        k (int): number of top feature interactions to show
+        seed (int): seed for the random number generator
+        cpu_time (int): maximum time to run the algorithm
+        verbose (bool): whether to print the results
+
+    Returns:
+        pandas.DataFrame : the bias weights as list for y_true = 0 and y_true = 1
+
+    """
+
+    y_values = y_true.unique()    
+    results = []
+    bias_values = []
+    for y_val in y_values:
+        if(verbose):
+            print("\nc Explaining for label", y_val)
+        result, bias = explain_statistical_parity(
+            clf,
+            X[y_true == y_val],
+            sensitive_features,
+            maxorder=maxorder,
+            spline_intervals=spline_intervals,
+            k=k,
+            seed=seed,
+            cpu_time=cpu_time,
+            verbose=verbose
         )
-        plt.tight_layout()
-        plt.show()
 
-    return result
+        results.append(result)
+        bias_values.append(bias)
+
+    return results, bias_values
+
+
+def explain_predictive_parity(
+        clf,
+        X,
+        y_true,
+        sensitive_features,
+        maxorder=2,
+        spline_intervals=5,
+        k=7,
+        seed=22,
+        cpu_time=300,
+        verbose=False):
+    """
+        Explain the predictive parity of a classifier given a dataset
+
+    Args:
+        clf: the trained model to explain
+            clf.predict() should be implemented
+        X (pandas.DataFrame): the dataset (containing the features) to measure the bias on
+        y_true (pandas.Series): the labels of the dataset
+        sensitive_features (list): list of sensitive attributes
+        maxorder (int): maximum order of the intersectionality of features
+        spline_intervals (int): number of intervals to use for the cubic spline approximation
+        k (int): number of top feature interactions to show
+        seed (int): seed for the random number generator
+        cpu_time (int): maximum time to run the algorithm
+        verbose (bool): whether to print the results
+
+    Returns:
+        pandas.DataFrame : the bias weights as list for y_predicted = 0 and y_predicted = 1
+    """
+
+    y_predicted = clf.predict(X)
+    y_values = np.unique(y_predicted)
+    results = []
+    bias_values = []
+    for y_val in y_values:
+        if(verbose):
+            print("\nc Explaining for label", y_val)
+        fairXplainer = FairXplainer(
+            None, 
+            X[y_predicted == y_val],
+            sensitive_features,
+            label=y_true[y_predicted == y_val]
+        )
+    
+        fairXplainer.compute(
+                maxorder=maxorder, 
+                spline_intervals=spline_intervals,
+                explain_sufficiency_fairness=True,
+                seed=seed,
+                compute_sp_only=False,
+                cpu_time=cpu_time, 
+                verbose=verbose)
+        
+        result = fairXplainer.get_top_k_weights(k=k)
+
+        results.append(result)
+        bias_values.append(fairXplainer.statistical_parity_sample())
+
+    return results, bias_values
+
+
+def draw_plot(result, **kwargs):
+    """
+        Draw the plot of the bias weights
+
+    Args:
+        result (pandas.DataFrame): the bias weights
+        kwargs: arguments to pass to the plot function
+
+    Returns:
+        matplotlib.pyplot : the plot
+    """
+
+    plt = fif_plot(result, 
+            **kwargs
+    )
+    plt.tight_layout()
+    return plt
 
     
