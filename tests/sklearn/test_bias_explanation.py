dc15455f0cf8fb405793a429b7337c3dc2ba6065
bishwamittra <bishwa12buet@gmail.com>
2023-05-03 06:59:43 +0200

    pytest with fixture
    Signed-off-by: bishwamittra <bishwa12buet@gmail.com>

--- tests/sklearn/test_bias_explanation.py
+++ tests/sklearn/test_bias_explanation.py
@@ -7,142 +7,194 @@ from aif360.sklearn.explainers import fairxplain_statistical_parity, fairxplain_
 from aif360.sklearn.metrics import intersection, selection_rate
 import numpy as np
 
-
-prot_attr_config_dic = {
-    "adult": {
-        0 : ['race'],
-        1 : ['sex'], 
-        2 : ['race', 'sex']
-    },
-    "compas": {
-        0 : ['race'],
-        1 : ['sex'],
-        2 : ['race', 'sex']
-    },
-    "german": {
-        0 : ['sex'],
-        1 : ['age'],
-        2 : ['age', 'sex']
-    }
-}
-
-def get_dataset(dataset, prot_attr):
-    if(dataset == "adult"):
-        X, y, sample_weight = fetch_adult(numeric_only=True)
-    elif(dataset == "compas"):
-        X, y = fetch_compas(numeric_only=True, binary_race=True)
-    elif(dataset == "german"):
-        X, y = fetch_german(numeric_only=True)
-        # make age binary if age is used as protected attribute
-        if('age' in prot_attr):
-            X['age'] = X['age'] > 25
-    else:
-        raise ValueError("Unknown dataset")
-    return X, y
-
-
-def get_statistical_parity_difference_from_aif360(X, y, prot_attr, clf):
-    positive_prediction_probabilities = np.array(
-        intersection(selection_rate, y, clf.predict(X), prot_attr=prot_attr)) # considering intersectional protected attributes
-    value_statistical_parity = positive_prediction_probabilities.max() - positive_prediction_probabilities.min()
-    return value_statistical_parity
-
-def get_equalized_odds_difference_from_aif360(X, y, prot_attr, clf):
-    value_equalized_odds = []
-    for each_y in y.unique():
-        positive_prediction_probabilities = np.array(
-            intersection(selection_rate, y[y == each_y], clf.predict(X[y == each_y]), prot_attr=prot_attr)) # considering intersectional protected attributes
-        value_statistical_parity = positive_prediction_probabilities.max() - positive_prediction_probabilities.min()
-        value_equalized_odds.append(value_statistical_parity)
-
-    # taking the max
-    value_equalized_odds = np.max(value_equalized_odds)
-    return value_equalized_odds
+import pytest
 
 
 
-def _test(dataset):
-    for prot_attr_config in [0, 1, 2]:
-        prot_attr = prot_attr_config_dic[dataset][prot_attr_config]
-        X, y = get_dataset(dataset, prot_attr)
-
-        # check if protected attributes are in the dataset
-        for attr in prot_attr:
-            assert attr in X.columns
+class FairnessInstance:
+    def __init__(self, dataset, prot_attr_config, classifier):
+        self.dataset = dataset
+        self.classifier = classifier
+        self.prot_attr_config = prot_attr_config
+        self.prot_attr = self.get_protected_attribute()
+        self.X, self.y = self.get_X_y()
 
         # learn a model
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        clf = LogisticRegression().fit(X_train, y_train)
+        self.clf = LogisticRegression().fit(self.X, self.y)
+
+        # compute fairness metrics
+        self._get_statistical_parity_difference_from_aif360()
+        self._get_equalized_odds_difference_from_aif360()
 
 
-        result, bias = fairxplain_statistical_parity(
-            clf, 
-            X_train, 
-            prot_attr=prot_attr, 
+        
+    def get_protected_attribute(self):
+        prot_attr_config_dic = {
+            "adult": {
+                0 : ['race'],
+                1 : ['sex'], 
+                2 : ['race', 'sex']
+            },
+            "compas": {
+                0 : ['race'],
+                1 : ['sex'],
+                2 : ['race', 'sex']
+            },
+            "german": {
+                0 : ['sex'],
+                1 : ['age'],
+                2 : ['age', 'sex']
+            }
+        }
+        return prot_attr_config_dic[self.dataset][self.prot_attr_config]
+    
+    def get_X_y(self):
+        if(self.dataset == "adult"):
+            X, y, sample_weight = fetch_adult(numeric_only=True)
+        elif(self.dataset == "compas"):
+            X, y = fetch_compas(numeric_only=True, binary_race=True)
+        elif(self.dataset == "german"):
+            X, y = fetch_german(numeric_only=True)
+            # make age binary if age is used as protected attribute
+            if('age' in self.prot_attr):
+                X['age'] = X['age'] > 25
+        else:
+            raise ValueError("Unknown dataset")
+        return X, y
+    
+
+    # helper function to compute statistical parity difference
+    def _get_statistical_parity_difference_from_aif360(self):
+        positive_prediction_probabilities = np.array(
+            intersection(selection_rate, self.y, self.clf.predict(self.X), prot_attr=self.prot_attr)) # considering intersectional protected attributes
+        self.value_statistical_parity = positive_prediction_probabilities.max() - positive_prediction_probabilities.min()
+
+    # helper function to compute predictive parity difference
+    def _get_equalized_odds_difference_from_aif360(self):
+        self.value_equalized_odds = []
+        for each_y in self.y.unique():
+            positive_prediction_probabilities = np.array(
+                intersection(selection_rate, self.y[self.y == each_y], self.clf.predict(self.X[self.y == each_y]), prot_attr=self.prot_attr)) # considering intersectional protected attributes
+            value_statistical_parity = positive_prediction_probabilities.max() - positive_prediction_probabilities.min()
+            self.value_equalized_odds.append(value_statistical_parity)
+
+        # taking the max
+        self.value_equalized_odds = np.max(self.value_equalized_odds)
+        
+    
+class FairXplainerInstance:
+
+    def __init__(self, X, y, prot_attr, clf):
+        self.X = X
+        self.y = y
+        self.prot_attr = prot_attr
+        self.clf = clf
+
+        # compute explanations
+        self._explain_statistical_parity()
+        self._explain_equalized_odds()
+        self._explain_predictive_parity()
+
+    # individual explanations
+    def _explain_statistical_parity(self):
+        self.result_statistical_parity, self.bias_statistical_parity = fairxplain_statistical_parity(
+            self.clf, 
+            self.X, 
+            prot_attr=self.prot_attr, 
             maxorder=1, 
             spline_intervals=3, 
-            verbose=True, 
+            verbose=False, 
             seed=None, 
             return_bias=True 
         )
 
-        # check if the bias value is correct 
-        assert bias == get_statistical_parity_difference_from_aif360(X_train, y_train, prot_attr, clf)
-
-        # check if result contains attributes that are in the dataset. Only works when maxorder=1
-        for attribute in list(result.index):
-            if(attribute == "Residual FIFs"):
-                continue
-            assert attribute in X.columns
-
-        results, bias_values = fairxplain_equalized_odds(
-            clf, 
-            X_train, 
-            y_train, # For equalized odds, we need to pass the true labels as well
-            prot_attr=prot_attr, 
-            maxorder=1,
-            spline_intervals=3,
-            seed=None,
-            verbose=False,
-            return_bias=True)
-        
-        # check if the bias value is correct
-        index_max_unfairness = np.argmax(bias_values) # we choose the class label with the highest bias for equalized odds
-        assert bias_values[index_max_unfairness] == get_equalized_odds_difference_from_aif360(X_train, y_train, prot_attr, clf)
-
-
-
-        # check if result contains attributes that are in the dataset. Only works when maxorder=1
-        for attribute in list(results[index_max_unfairness].index):
-            if(attribute == "Residual FIFs"):
-                continue
-            assert attribute in X.columns
-
-        results, bias_values = fairxplain_predictive_parity(
-            clf, 
-            X_train, 
-            y_train, # For predictive parity, we need to pass the true labels as well
-            prot_attr=prot_attr, 
+    def _explain_equalized_odds(self):
+        self.result_equalized_odds, self.bias_equalized_odds = fairxplain_equalized_odds(
+            self.clf, 
+            self.X, 
+            self.y, 
+            prot_attr=self.prot_attr, 
             maxorder=1, 
-            spline_intervals=3,
-            seed=None,
-            verbose=False,
-            return_bias=True)
-        
-        # check if result contains attributes that are in the dataset. Only works when maxorder=1
-        index_max_unfairness = np.argmax(bias_values)
-        for attribute in list(results[index_max_unfairness].index):
-            if(attribute == "Residual FIFs"):
+            spline_intervals=3, 
+            verbose=False, 
+            seed=None, 
+            return_bias=True 
+        )
+
+    def _explain_predictive_parity(self):
+        self.result_predictive_parity, self.bias_predictive_parity = fairxplain_predictive_parity(
+            self.clf, 
+            self.X, 
+            self.y, 
+            prot_attr=self.prot_attr, 
+            maxorder=1, 
+            spline_intervals=3, 
+            verbose=False, 
+            seed=None, 
+            return_bias=True
+        )
+
+
+@pytest.fixture(params=[
+    ["compas", 0, LogisticRegression()],
+    ["compas", 1, LogisticRegression()],
+    ["compas", 2, LogisticRegression()],
+    ["adult", 0, LogisticRegression()],
+    ["adult", 1, LogisticRegression()],
+    ["adult", 2, LogisticRegression()],
+    ["german", 0, LogisticRegression()],
+    ["german", 1, LogisticRegression()],
+    ["german", 2, LogisticRegression()],
+])
+def fair_instance(request):
+    return FairnessInstance(request.param[0], request.param[1], request.param[2])
+
+@pytest.fixture
+def fairxplainer_instance(fair_instance):
+    return FairXplainerInstance(fair_instance.X, fair_instance.y, fair_instance.prot_attr, fair_instance.clf)
+
+def test_attribute_contains_special_characters(fair_instance):
+    # check if protected attributes contain special characters: &
+    for attribute in fair_instance.X.columns:
+        assert '&' not in attribute
+
+def test_protected_attribute(fair_instance):
+    # check if protected attributes are in the dataset
+    assert all(prot_attr in fair_instance.X.columns for prot_attr in fair_instance.prot_attr)
+
+def test_model_is_trained(fair_instance):
+    # check if model is trained
+    assert fair_instance.clf is not None
+    assert fair_instance.clf.predict(fair_instance.X).shape[0] == fair_instance.X.shape[0]
+
+def test_bias_correct_statistical_parity(fair_instance, fairxplainer_instance):
+    # check if bias is correct
+    assert fairxplainer_instance.bias_statistical_parity == fair_instance.value_statistical_parity
+
+def test_bias_correct_equalized_odds(fair_instance, fairxplainer_instance):
+    # check if bias is correct
+    assert np.max(fairxplainer_instance.bias_equalized_odds) == fair_instance.value_equalized_odds
+
+def test_coherent_explanation(fairxplainer_instance):
+    # check if explanations are coherent.
+    # For now we check whether the attribute matches original dataset columns
+
+    # statistical parity
+    for attribute in fairxplainer_instance.result_statistical_parity.index:
+        if(attribute == "Residual FIFs"):
                 continue
-            assert attribute in X.columns
+        assert attribute in fairxplainer_instance.X.columns
 
-        
+    # equalized odds
+    for result in fairxplainer_instance.result_equalized_odds:
+        for attribute in result.index:
+            if(attribute == "Residual FIFs"):
+                    continue
+            assert attribute in fairxplainer_instance.X.columns
 
-def test_all():
-    for dataset in ["compas", "german", "adult"]:
-        _test(dataset)
-        
-        
-
-
-
+    # predictive parity
+    for result in fairxplainer_instance.result_predictive_parity:
+        for attribute in result.index:
+            if(attribute == "Residual FIFs"):
+                    continue
+            assert attribute in fairxplainer_instance.X.columns
