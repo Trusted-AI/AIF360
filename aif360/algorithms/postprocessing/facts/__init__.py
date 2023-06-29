74eb0593de262d824b80ce288cb62503111717e3
phantom-duck <37215464+phantom-duck@users.noreply.github.com>
2023-06-29 15:42:28 +0300

    FACTS: Added doc to top-level wrapper class
    Signed-off-by: phantom-duck <37215464+phantom-duck@users.noreply.github.com>

--- aif360/algorithms/postprocessing/facts/__init__.py
+++ aif360/algorithms/postprocessing/facts/__init__.py
@@ -11,6 +11,20 @@ from .formatting import print_recourse_report, print_recourse_report_cumulative
 __all__ = ["FairnessAwareSubgroupCounterfactuals"]
 
 class FairnessAwareSubgroupCounterfactuals:
+    """FACTS is an efficient, model-agnostic, highly parameterizable, and
+    explainable framework for evaluating subgroup fairness through
+    counterfactual explanations [#FACTS23]_.
+
+    This class is a wrapper for the various methods exposed by the
+    FACTS framework.
+
+    References:
+        .. [#FACTS23] `L. Kavouras, K. Tsopelas, G. Giannopoulos,
+           D. Sacharidis, E. Psaroudaki, N. Theologitis, D. Rontogiannis,
+           D. Fotakis, I. Emiris, "Fairness Aware Counterfactuals for
+           Subgroups", arXiv preprint, 2023.
+           <https://arxiv.org/abs/2306.14978>`_
+    """
     ParameterProxy = ParameterProxy
     feature_change_builder = feature_change_builder
     valid_ifthens_with_coverage_correctness = valid_ifthens_with_coverage_correctness
