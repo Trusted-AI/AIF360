7ea6b476ff64c61a81d70bc507869f02bf063b62
NiloyPurkait <purkait.pur@gmail.com>
2023-08-18 16:09:01 +0200

    MAAD algorithm
    1) Included "multi_attribute_adversarial_debiasing" algorithm in algorithms.inprocessing directory
    
    2) Added notebook in test directory
    
    Signed-off-by: NiloyPurkait <purkait.pur@gmail.com>

---
+++ aif360/algorithms/inprocessing/multi_attribute_adversarial_debiasing.py
@@ -0,0 +1,311 @@
+import tensorflow as tf
+import os
+import numpy as np
+from aif360.metrics import BinaryLabelDatasetMetric
+from aif360.metrics import ClassificationMetric
+from tensorflow.keras.layers import Dense, Dropout, Activation, Concatenate
+from tensorflow.keras.initializers import glorot_uniform
+
+
+
+class ClassifierModel(tf.keras.Model):
+    def __init__(self, classifier_num_hidden_units=256, seed=None):
+        super(ClassifierModel, self).__init__(name='classifier')
+
+        self.dense_layer_1 = Dense(classifier_num_hidden_units,
+                                   activation='relu',
+                                   kernel_initializer=glorot_uniform(seed=seed))
+        self.dropout_layer = Dropout(0.5)
+        self.dense_layer_2 = Dense(classifier_num_hidden_units, activation='relu',
+                                   kernel_initializer=glorot_uniform(seed=seed))
+        self.output_logit_layer = Dense(1, activation=None)
+        self.output_label_activation = Activation('sigmoid')
+        
+    def call(self, features):
+        x = self.dense_layer_1(features)
+        x = self.dropout_layer(x)
+        x = self.dense_layer_2(x)
+        pred_logit = self.output_logit_layer(x)
+        pred_label = self.output_label_activation(pred_logit)
+        return pred_logit
+
+
+
+class AdversaryModel(tf.keras.Model):
+    def __init__(self, seed=None, nb_sensitive_features=2):
+        super(AdversaryModel, self).__init__(name='adversary')
+        self.c = self.add_weight('c', shape=(), initializer='ones')
+        self.dense_layer = Dense(64,
+                                 activation='relu',
+                                 kernel_initializer=glorot_uniform(seed=seed))
+        self.output_logit_layer = Dense(nb_sensitive_features, activation=None)
+        self.output_label_activation = Activation('sigmoid')
+        
+    def call(self, pred_logits, true_labels):
+        s = tf.keras.activations.sigmoid((1 + tf.abs(self.c)) * pred_logits)
+        x = Concatenate(axis=1)([s, s * true_labels, s * (1.0 - true_labels)])
+        x = self.dense_layer(x)
+        pred_protected_attribute_logits = self.output_logit_layer(x)
+        pred_protected_attribute_labels = self.output_label_activation(pred_protected_attribute_logits)
+        return pred_protected_attribute_labels, pred_protected_attribute_logits
+
+
+
+
+class WeightedBinaryCrossEntropy(tf.keras.losses.Loss):
+    '''Loss function for adversary. Weights can be provided for each sensitive attribute.'''
+    def __init__(self, weights, **kwargs):
+        super(WeightedBinaryCrossEntropy, self).__init__(**kwargs)
+        self.weights = weights
+
+    def call(self, y_true, y_pred):
+        epsilon = 1e-7
+        y_pred = tf.clip_by_value(y_pred, epsilon, 1.0 - epsilon)
+        losses = []
+        for i, weight in enumerate(self.weights):
+            loss = -tf.reduce_mean(y_true[:, i] * tf.math.log(y_pred[:, i]) + (1.0 - y_true[:, i]) * tf.math.log(1.0 - y_pred[:, i]))
+            losses.append(weight * loss)
+        return tf.reduce_sum(losses)
+
+
+
+class AdversarialDebiasor:
+    '''TODO: 
+    
+    # Make train_step() faster
+    # 2. Make different types of adversaries.
+        
+        Depending on the definition of fairness being achieved, the adversary may have other inputs.
+
+            • For DEMOGRAPHIC PARITY, the adversary gets the predicted label Yˆ . Intuitively, this allows the adversary to try
+              to predict the protected variable using nothing but the predicted label. The goal of the predictor is to prevent the
+              adversary from doing this.
+
+            • For EQUALITY OF ODDS, the adversary gets Yˆ and the true label Y .
+
+            • For EQUALITY OF OPPORTUNITY on a given class y, we can restrict the training set of the adversary to training examples where Y = y.
+ 
+       References:
+            [1] B. H. Zhang, B. Lemoine, and M. Mitchell, "Mitigating UnwantedBiases with Adversarial Learning," 
+            AAAI/ACM Conference on Artificial Intelligence, Ethics, and Society, 2018.
+ 
+ '''
+    def __init__(self, classifier=ClassifierModel(),
+                  adversary=AdversaryModel(),
+                    loss_weights = [.5, .5],
+                      classifier_optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+                        adversary_optimizer=tf.keras.optimizers.Adam(learning_rate=0.001)):
+        self.classifier = classifier
+        self.adversary = adversary
+        self.loss_fn = WeightedBinaryCrossEntropy(weights=loss_weights)
+        self.classifier_optimizer = classifier_optimizer
+        self.adversary_optimizer = adversary_optimizer
+        self.debias = True
+        self.adversary_loss_weight = 0.1
+        if 'tensorflow_privacy' in str(classifier_optimizer):
+            self.differential_privacy=True
+        else:
+            self.differential_privacy=False
+        
+
+    def pretrain_classifier(self, features, labels, num_epochs, batch_size):
+        num_samples = features.shape[0]
+        for epoch in range(num_epochs):
+            permuted_indices = np.random.permutation(num_samples)
+            for i in range(0, num_samples, batch_size):
+                batch_indices = permuted_indices[i:i+batch_size]
+                batch_features = tf.convert_to_tensor(features[batch_indices], dtype=tf.float32)
+                batch_labels = tf.convert_to_tensor(labels[batch_indices], dtype=tf.float32)
+
+                def loss_fn():
+                    pred_logits = self.classifier(batch_features)
+                    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch_labels, logits=pred_logits))
+                
+                # Use gradient tape to compute the gradients
+                with tf.GradientTape() as tape:
+                    loss_value = loss_fn()
+                    # Use compute_gradients with a callable loss function and pass the gradient tape
+                    if self.differential_privacy:
+                        grads_and_vars = self.classifier_optimizer.compute_gradients(loss_value, self.classifier.trainable_variables, gradient_tape=tape)
+                        self.classifier_optimizer.apply_gradients(grads_and_vars)
+                    else:
+                        grads = tape.gradient(loss_value, self.classifier.trainable_variables)
+                        self.classifier_optimizer.apply_gradients(zip(grads, self.classifier.trainable_variables))
+                    
+            
+            print(f"Pretraining Classifier - Epoch {epoch+1}, Loss: {loss_fn().numpy()}")
+
+
+
+
+    def pretrain_adversary(self, features, labels, protected_attributes, num_epochs, batch_size):
+        num_samples = features.shape[0]
+        for epoch in range(num_epochs):
+            permuted_indices = np.random.permutation(num_samples)
+            for i in range(0, num_samples, batch_size):
+                batch_indices = permuted_indices[i:i+batch_size]
+                batch_features = tf.convert_to_tensor(features[batch_indices], dtype=tf.float32)
+                batch_labels = tf.convert_to_tensor(labels[batch_indices], dtype=tf.float32)
+                batch_protected_attributes = tf.convert_to_tensor(protected_attributes[batch_indices], dtype=tf.float32)
+                
+                pred_logits = self.classifier(batch_features)
+                
+                with tf.GradientTape() as tape:
+                    pred_protected_attribute_labels, pred_protected_attribute_logits = self.adversary(pred_logits, batch_labels)
+                    adversary_loss = self.loss_fn(batch_protected_attributes, pred_protected_attribute_labels)
+                grads = tape.gradient(adversary_loss, self.adversary.trainable_variables)
+                self.adversary_optimizer.apply_gradients(zip(grads, self.adversary.trainable_variables))
+            
+            print(f"Pretraining Adversary - Epoch {epoch+1}, Loss: {adversary_loss.numpy()}")
+
+
+    #@tf.function
+    def train_step_(self, batch_features, batch_labels, batch_protected_attributes):
+        with tf.GradientTape(persistent=True) as tape:
+            pred_logits = self.classifier(batch_features)
+            classifier_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch_labels, logits=pred_logits))
+            
+            if self.debias:
+                pred_protected_attribute_labels, pred_protected_attribute_logits = self.adversary(pred_logits, batch_labels)
+                adversary_loss = self.loss_fn(batch_protected_attributes, pred_protected_attribute_labels)
+
+        classifier_vars = self.classifier.trainable_variables
+        classifier_grads = tape.gradient(classifier_loss, classifier_vars)
+
+        if self.debias:
+            adversary_vars = self.adversary.trainable_variables
+            adversary_grads = tape.gradient(adversary_loss, classifier_vars)
+            adversary_grads_dict = {var.name: grad for (grad, var) in zip(adversary_grads, classifier_vars)}
+
+            normalize = lambda x: x / (tf.norm(x) + np.finfo(np.float32).tiny)
+            updated_classifier_grads = []
+            for (grad, var) in zip(classifier_grads, classifier_vars):
+                if var.name in adversary_grads_dict:
+                    unit_adversary_grad = normalize(adversary_grads_dict[var.name])
+                    grad -= tf.reduce_sum(grad * unit_adversary_grad) * unit_adversary_grad
+                    grad -= self.adversary_loss_weight * adversary_grads_dict[var.name]
+                updated_classifier_grads.append((grad, var))
+
+            self.classifier_optimizer.apply_gradients(updated_classifier_grads)
+            self.adversary_optimizer.apply_gradients(zip(adversary_grads, adversary_vars))
+
+        return classifier_loss, adversary_loss
+
+
+
+    
+    def train_step(self, batch_features, batch_labels, batch_protected_attributes):
+        ''' INFO: Training step for the model'''
+
+        # Create a GradientTape to automatically track operations and compute gradients
+        with tf.GradientTape(persistent=True) as tape:
+            # Get the predicted logits from the classifier
+            pred_logits = self.classifier(batch_features)
+            # Compute the classifier loss using sigmoid cross-entropy
+            classifier_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=batch_labels, logits=pred_logits))
+            # If debiasing is enabled, compute the adversary loss
+            if self.debias:
+                # Get the predicted labels and logits for the protected attributes from the adversary
+                pred_protected_attribute_labels, pred_protected_attribute_logits = self.adversary(pred_logits, batch_labels)
+                # Compute the adversary loss using the specified loss function
+                adversary_loss = self.loss_fn(batch_protected_attributes, pred_protected_attribute_labels)
+
+        # Get the list of trainable variables in the classifier
+        classifier_vars = [var for var in self.classifier.trainable_variables]
+
+        # If debiasing is enabled, perform additional computations
+        if self.debias:
+            # Get the list of trainable variables in the adversary
+            adversary_vars = [var for var in self.adversary.trainable_variables]
+            # Compute the gradients of the adversary loss with respect to the classifier variables
+            adversary_grads = {var.name: grad for (grad, var) in zip(tape.gradient(adversary_loss, classifier_vars), classifier_vars)}
+
+            # Define a normalization function to normalize a tensor
+            normalize = lambda x: x / (tf.norm(x) + np.finfo(np.float32).tiny)
+
+            # Initialize a list to store the updated classifier gradients
+            updated_classifier_grads = []
+            # For each gradient and variable in the classifier
+            for (grad, var) in zip(tape.gradient(classifier_loss, classifier_vars), classifier_vars):
+                # If the variable is present in the adversary gradients
+                if var.name in adversary_grads:
+                    # Normalize the corresponding adversary gradient
+                    unit_adversary_grad = normalize(adversary_grads[var.name])
+                    # Subtract the projection of the classifier gradient onto the normalized adversary gradient
+                    grad -= tf.reduce_sum(grad * unit_adversary_grad) * unit_adversary_grad
+                    # Subtract the weighted adversary gradient
+                    grad -= self.adversary_loss_weight * adversary_grads[var.name]
+                # Append the updated gradient and variable to the list
+                updated_classifier_grads.append((grad, var))
+
+            # Apply the updated gradients to the classifier variables
+            self.classifier_optimizer.apply_gradients(updated_classifier_grads)
+
+            # If debiasing is enabled, update the adversary variables
+            if self.debias:
+                # Ensure the classifier is updated before the adversary
+                with tf.control_dependencies([self.classifier_optimizer.apply_gradients(updated_classifier_grads)]):
+                    # Minimize the adversary loss with respect to the adversary variables
+                    self.adversary_optimizer.minimize(adversary_loss, var_list=adversary_vars, tape=tape)
+
+        # Return the classifier and adversary losses
+        return classifier_loss, adversary_loss
+
+
+
+
+
+
+
+    def train(self, features, labels, protected_attributes, num_epochs, batch_size):
+        num_samples = features.shape[0]
+        for epoch in range(num_epochs):
+            permuted_indices = np.random.permutation(num_samples)
+            for i in range(0, num_samples, batch_size):
+                batch_indices = permuted_indices[i:i+batch_size]
+                batch_features = tf.convert_to_tensor(features[batch_indices], dtype=tf.float32)
+                batch_labels = tf.convert_to_tensor(labels[batch_indices], dtype=tf.float32)
+                batch_protected_attributes = tf.convert_to_tensor(protected_attributes[batch_indices], dtype=tf.float32)
+                
+                classifier_loss, adversary_loss = self.train_step(batch_features, batch_labels, batch_protected_attributes)
+            
+            print(f"Epoch {epoch+1}, Classifier Loss: {classifier_loss.numpy()}, Adversary Loss: {adversary_loss.numpy()}")
+
+
+    def predict_proba(self, X):
+        logits = self.classifier(X)
+        return tf.nn.sigmoid(logits)
+    
+    def predict(self, X, threshold=.5):
+        return tf.cast(self.predict_proba(X) >= threshold, dtype=tf.int32)
+    
+    def transform_dataset(self, dataset):
+        preds = self.predict_proba(dataset.features)
+        # Mutated, fairer dataset with new labels
+        dataset_new = dataset.copy(deepcopy = True)
+        dataset_new.scores = np.array(preds, dtype=np.float64).reshape(-1, 1)
+        dataset_new.labels = (np.array(preds)>0.5).astype(np.float64).reshape(-1,1)
+
+        return dataset_new
+    
+    
+    def get_dataset_metrics(self, dataset):
+        metrics = {s:'' for s in dataset.protected_attribute_names}
+        for attr in dataset.protected_attribute_names:
+            unprivileged_group, privileged_group = {attr:0}, {attr:1}
+            new_dataset = self.transform_dataset(dataset)
+            metrics[attr] = BinaryLabelDatasetMetric(new_dataset, 
+                                                     unprivileged_groups=[unprivileged_group],
+                                                     privileged_groups=[privileged_group])
+        return metrics
+
+    def get_classification_metrics(self, dataset):
+        metrics = {s:'' for s in dataset.protected_attribute_names}
+        for attr in dataset.protected_attribute_names:
+            unprivileged_group, privileged_group = {attr:0}, {attr:1}
+            new_dataset = self.transform_dataset(dataset)
+            metrics[attr] = ClassificationMetric(dataset, 
+                                                 new_dataset,
+                                                 unprivileged_groups=[unprivileged_group],
+                                                 privileged_groups=[privileged_group])
+        return metrics
--- aif360/sklearn/metrics/metrics.py
+++ aif360/sklearn/metrics/metrics.py
@@ -1,21 +1,18 @@
 from itertools import permutations
-from typing import Union
 
 import numpy as np
 import pandas as pd
 from scipy.special import rel_entr
-from sklearn.metrics import make_scorer as _make_scorer, recall_score, precision_score
+from sklearn.metrics import make_scorer as _make_scorer, recall_score
 from sklearn.metrics import multilabel_confusion_matrix
 from sklearn.metrics._classification import _prf_divide, _check_zero_division
 from sklearn.neighbors import NearestNeighbors
 from sklearn.utils import check_X_y
 from sklearn.utils.deprecation import deprecated
 
-from aif360.metrics import ot_metric
 from aif360.sklearn.utils import check_inputs, check_groups
 from aif360.detectors.mdss.ScoringFunctions import BerkJones, Bernoulli
 from aif360.detectors.mdss.MDSS import MDSS
-
 
 __all__ = [
     # meta-metrics
@@ -27,8 +24,8 @@ __all__ = [
     'specificity_score', 'base_rate', 'selection_rate', 'smoothed_base_rate',
     'smoothed_selection_rate', 'generalized_fpr', 'generalized_fnr',
     # group fairness
-    'ot_distance', 'statistical_parity_difference', 'disparate_impact_ratio',
-    'equal_opportunity_difference', 'average_odds_difference', 'average_predictive_value_difference',
+    'statistical_parity_difference', 'disparate_impact_ratio',
+    'equal_opportunity_difference', 'average_odds_difference',
     'average_odds_error', 'class_imbalance', 'kl_divergence',
     'conditional_demographic_disparity', 'smoothed_edf',
     'df_bias_amplification', 'mdss_bias_scan', 'mdss_bias_score',
@@ -347,27 +344,6 @@ def specificity_score(y_true, y_pred, *, pos_label=1, sample_weight=None,
     return _prf_divide(tn, negs, 'specificity', 'negative', None,
                        ('specificity',), zero_division).item()
 
-def false_omission_rate_error(y_true, y_pred, *, pos_label=1, sample_weight=None,
-                      zero_division='warn'):
-    """Compute the false omission rate.
-
-    Args:
-        y_true (array-like): Ground truth (correct) target values.
-        y_pred (array-like): Estimated targets as returned by a classifier.
-        pos_label (scalar, optional): The label of the positive class.
-        sample_weight (array-like, optional): Sample weights.
-        zero_division ('warn', 0 or 1): Sets the value to return when there is a
-            zero division. If set to “warn”, this acts as 0, but warnings are
-            also raised.
-    """
-    _check_zero_division(zero_division)
-    MCM = multilabel_confusion_matrix(y_true, y_pred, labels=[pos_label],
-                                      sample_weight=sample_weight)
-    tn, fn = MCM[:, 0, 0], MCM[:, 1, 0]
-    negs = tn + fn
-    return _prf_divide(fn, negs, 'false omission rate', 'predicted negative', None,
-                       ('false omission rate',), zero_division).item()
-
 def base_rate(y_true, y_pred=None, *, pos_label=1, sample_weight=None):
     r"""Compute the base rate, :math:`Pr(Y = \text{pos_label}) = \frac{P}{P+N}`.
 
@@ -502,63 +478,6 @@ def generalized_fnr(y_true, probas_pred, *, pos_label=1, sample_weight=None,
 
 
 # ============================ GROUP FAIRNESS ==================================
-def ot_distance(
-    y_true: pd.Series,
-    y_pred: Union[pd.Series, pd.DataFrame],
-    prot_attr: pd.Series = None,
-    pos_label: Union[str, float] = None,
-    scoring: str = "Wasserstein1",
-    num_iters: int = 1e5,
-    penalty: float = 1e-17,
-    mode: str = "binary",
-    cost_matrix: np.ndarray=None,
-    **kwargs,
-):
-    """Normalize and calculate Wasserstein distance between groups defined by `prot_attr` in `y_true` and `y_pred`.
-
-    Args:
-        y_true (pd.Series): ground truth (correct) target values.
-        y_pred (pd.Series, pd.DataFrame): estimated target values.
-            If `mode` is nominal, must be a `pd.DataFrame` with columns containing predictions for each nominal class,
-                or list of corresponding column names in `data`.
-            If `None`, model is assumed to be a dummy model that predicts the mean of the targets
-                or 1/(number of categories) for nominal mode.
-        prot_attr (pd.Series): sensitive attribute values.
-            If `None`, assume all samples belong to the same protected group.
-        pos_label(str, float, optional): Either "high", "low" or a float value if the mode in [binary, ordinal, or continuous].
-                If float, value has to be the minimum or the maximum in the ground_truth column.
-                Defaults to high if None for these modes.
-                Support for float left in to keep the intuition clear in binary classification tasks.
-                If `mode` is nominal, favorable values should be one of the unique categories in the ground_truth.
-                Defaults to a one-vs-all scan if None for nominal mode.
-        scoring (str or class): only 'Wasserstein1'
-        num_iters (int, optional): number of iterations (random restarts) for EMD. Should be positive.
-        penalty (float, optional): penalty term. Should be positive. The penalty term as with any regularization parameter
-            may need to be tuned for a particular use case. The higher the penalty, the higher the influence of entropy regualizer.
-        mode: one of ['binary', 'continuous', 'nominal', 'ordinal']. Defaults to binary.
-                In nominal mode, up to 10 categories are supported by default.
-                To increase this, pass in keyword argument max_nominal = integer value.
-        cost_matrix (np.ndarray): cost matrix for the Wasserstein distance. Defaults to absolute difference between samples.
-
-    Returns:
-        ot.emd2 (float, dict): Earth mover's distance or dictionary of optimal transports for each of option of classifier
-
-    Raises:
-        ValueError: if `mode` is 'binary' but `ground_truth` contains less than 1 or more than 2 unique values.
-    """
-    return ot_metric.ot_distance(
-        ground_truth=y_true,
-        classifier=y_pred,
-        prot_attr=prot_attr,
-        favorable_value=pos_label,
-        scoring=scoring,
-        num_iters=num_iters,
-        penalty=penalty,
-        mode=mode,
-        cost_matrix=cost_matrix,
-        **kwargs
-    )
-
 def statistical_parity_difference(y_true, y_pred=None, *, prot_attr=None,
                                   priv_group=1, pos_label=1, sample_weight=None):
     r"""Difference in selection rates.
@@ -729,37 +648,6 @@ def average_odds_error(y_true, y_pred, *, prot_attr=None, priv_group=None,
                           priv_group=priv_group, pos_label=pos_label,
                           sample_weight=sample_weight)
     return (abs(tpr_diff) + abs(fpr_diff)) / 2
-
-def average_predictive_value_difference(y_true, y_pred, *, prot_attr=None, priv_group=1,
-                            pos_label=1, sample_weight=None):
-    r"""Returns the average of the difference in positive predictive value and false omission rate for the unprivileged and privileged groups:
-
-    .. math::
-
-        \dfrac{(PPV_{D = \text{unprivileged}} - PPV_{D = \text{privileged}})
-        + (FOR_{D = \text{unprivileged}} - FOR_{D = \text{privileged}})}{2}
-
-    A value of 0 indicates equality of chance of success.
-
-    Args:
-        y_true (pandas.Series): Ground truth (correct) target values.
-        y_pred (array-like): Estimated targets as returned by a classifier.
-        prot_attr (array-like, keyword-only): Protected attribute(s). If
-            ``None``, all protected attributes in y_true are used.
-        priv_group (scalar, optional): The label of the privileged group.
-        pos_label (scalar, optional): The label of the positive class.
-        sample_weight (array-like, optional): Sample weights.
-
-    Returns:
-        float: Average predictive value difference.
-    """
-    for_diff = difference(false_omission_rate_error, y_true, y_pred,
-                           prot_attr=prot_attr, priv_group=priv_group,
-                           pos_label=pos_label, sample_weight=sample_weight)
-    ppv_diff = difference(precision_score, y_true, y_pred, prot_attr=prot_attr,
-                          priv_group=priv_group, pos_label=pos_label,
-                          sample_weight=sample_weight)
-    return (ppv_diff + for_diff) / 2
 
 def class_imbalance(y_true, y_pred=None, *, prot_attr=None, priv_group=1,
                     sample_weight=None):
