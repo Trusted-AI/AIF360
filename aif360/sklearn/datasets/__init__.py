131c42f5dbb31033829b46156b01176877d05b99
Ekta <ekta12bhaskar@gmail.com>
2023-10-12 17:18:48 -0400

    changes for the suggesttions in code review

--- aif360/sklearn/datasets/__init__.py
+++ aif360/sklearn/datasets/__init__.py
@@ -13,3 +13,4 @@ from aif360.sklearn.datasets.openml_datasets import fetch_adult, fetch_german, f
 from aif360.sklearn.datasets.compas_dataset import fetch_compas
 from aif360.sklearn.datasets.meps_datasets import fetch_meps
 from aif360.sklearn.datasets.tempeh_datasets import fetch_lawschool_gpa
+from aif360.sklearn.datasets.seaphe_datasets import load_lawschool_data
--- aif360/sklearn/datasets/seaphe_datasets.py
+++ aif360/sklearn/datasets/seaphe_datasets.py
@@ -54,8 +54,8 @@ def load_lawschool_data(target, subset="all", usecols=None, dropcols=None,
     X_train, X_test, y_train, y_test = train_test_split(
         All_features, 
         All_labels, 
-        test_size=0.2,
-        random_state=42
+        test_size= 0.33,
+        random_state=123
     )
     
     if subset == "train":
