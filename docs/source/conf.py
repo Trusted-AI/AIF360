9550bb2108573daff5f073dc3f5d6b8408117949
Samuel Hoffman <hoffman.sc@gmail.com>
2024-02-21 14:45:46 -0500

    Fix sphinx_rtd_theme
    Signed-off-by: Samuel Hoffman <hoffman.sc@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -130,15 +130,7 @@ strip_signature_backslash = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-# html_theme = 'bizstyle'
-if os.environ.get('READTHEDOCS') != 'True':
-    try:
-        import sphinx_rtd_theme
-    except ImportError:
-        pass  # assume we have sphinx >= 1.3
-    else:
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
